8066432e0076ba2708c06b46cb5e784f2036a330
maty zisserman <maty21@gmail.com>
2017-11-21 17:07:42 +0200

    github

--- repos.js
+++ repos.js
@@ -1,13 +1,24 @@
-const reposNames = ['hkube', 'simulator', 'pipeline-driver', 'api-server', 'worker', 'algoPackage', 'algorunner', 'common'];
-
-const commonReposNames = ['statistic-check.hkube', 'rest-server.hkube',
+const githubRepos = require('github-repositories');
+let reposNames = ['hkube', 'simulator', 'pipeline-driver', 'api-server', 'worker', 'algoPackage', 'algorunner', 'common'];
+let commonReposNames = ['statistic-check.hkube', 'rest-server.hkube',
     'producer-consumer.hkube', 'etcd.hkube', 'backoff.hkube', 'request-reply.hkube',
     'redis-utils.hkube', 'pub-sub-adapter.hkube', 'logger.hkube', 'elastic-client.hkube', 'config.hkube']
+
+
+const gitHubRepos = () => {
+    githubRepos('kube-HPC').then(data => {
+        let repos = data.map(r => r.name)
+        commonReposNames = repos.filter(r => r.split('.')[1] != null);
+        reposNames = repos.filter(r => r.split('.')[1] == null);
+        //=> [{id: 29258368, name: 'animal-sounds', full_name: 'kevva/animal-sounds', ...}, ...] 
+    });
+
+}
 
 
 module.exports = {
     reposNames,
     commonReposNames
 };
-
+gitHubRepos();
 
