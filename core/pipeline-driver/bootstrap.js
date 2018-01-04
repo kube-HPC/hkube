d35a6664f2e1556f4418890b86e82229b7357d07
yehiyam <yehiyam@gmail.com>
2018-01-04 09:39:46 +0200

    make config sync

--- core/pipeline-driver/bootstrap.js
+++ core/pipeline-driver/bootstrap.js
@@ -21,7 +21,7 @@ const modules = [
 class Bootstrap {
     async init() {
         try {
-            const { main, logger } = await configIt.load();
+            const { main, logger } = configIt.load();
             this._handleErrors();
 
             log = new Logger(main.serviceName, logger);
