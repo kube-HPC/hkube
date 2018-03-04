33723895027812f6503aa3fde967538962f141aa
yehiyam <yehiyam@gmail.com>
2018-03-04 12:56:05 +0200

    add optional repository for base image

--- core/worker/dockerfile/build.sh
+++ core/worker/dockerfile/build.sh
@@ -6,15 +6,20 @@ then
 else
   IMAGE_NAME=hkube/${REPO_NAME}
 fi
-
+echo npm_package_version=${npm_package_version}
 VERSION="v${npm_package_version}"
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST:-"false"}" != "false" ]; then
   VERSION=${VERSION}-${TRAVIS_PULL_REQUEST_BRANCH}-${TRAVIS_JOB_NUMBER}
 fi
 TAG_VER="${IMAGE_NAME}:${VERSION}"
 
-docker build -t ${TAG_VER} -f ./dockerfile/Dockerfile .
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ] || [ -z "${TRAVIS_PULL_REQUEST}" ]; then
+BASE_PRIVATE_REGISTRY=""
+if [ -v PRIVATE_REGISTRY ]
+then
+  BASE_PRIVATE_REGISTRY="${PRIVATE_REGISTRY}/"
+fi
+docker build -t ${TAG_VER} --build-arg BASE_PRIVATE_REGISTRY="${BASE_PRIVATE_REGISTRY}" -f ./dockerfile/Dockerfile .
+if [ "${TRAVIS_PULL_REQUEST:-"false"}" == "false" ] || [ -z "${TRAVIS_PULL_REQUEST}" ]; then
   TAG_CUR="${IMAGE_NAME}:latest"
   docker tag ${TAG_VER} "${TAG_CUR}"
 fi
