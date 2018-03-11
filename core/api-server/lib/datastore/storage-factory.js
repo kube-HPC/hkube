d4f021740d94f0cfe55d4eef2dcffc7aa89b1d7d
Nassi Harel <nassi9@gmail.com>
2018-03-11 16:21:09 +0200

    add storage module name to results

--- core/api-server/lib/datastore/storage-factory.js
+++ core/api-server/lib/datastore/storage-factory.js
@@ -1,6 +1,7 @@
 class StorageFactory {
     async init(config) {
         const storage = config.storageAdapters[config.defaultStorage];
+        this.moduleName = storage.moduleName;
         this.adapter = require(storage.moduleName);  // eslint-disable-line
         await this.adapter.init(storage.connection);
     }
@@ -8,6 +9,7 @@ class StorageFactory {
     async setResultsFromStorage(options) {
         if (options.data.result) {
             options.data.result = await Promise.all(options.data.result.map(a => this._getStorageItem(a)));
+            options.data.storageModule = this.moduleName;
         }
     }
 
