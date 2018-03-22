555fb7d228979a3f1a0ed59c3989c15f7e17b55c
Nassi Harel <nassi9@gmail.com>
2018-03-22 16:19:48 +0200

    Merge branch 'master' of github.com:kube-HPC/resource-manager

--- core/resource-manager/lib/adapters/k8s.js
+++ core/resource-manager/lib/adapters/k8s.js
@@ -1,4 +1,4 @@
-
+const groupBy = require('lodash.groupby');
 const Api = require('kubernetes-client');
 const Adapter = require('./Adapter');
 const parse = require('parseunit');
@@ -18,43 +18,61 @@ class K8sAdapter extends Adapter {
     async getData() {
         let nodes = await this._client.nodes.get();
         let pods = await this._client.pods.get();
-        let nodeSpec = new Map();
-        pods.items.forEach(pod => {
-            if (nodeSpec.has(pod.spec.nodeName)) {
-                let containers = nodeSpec.get(pod.spec.nodeName);
-                pod.spec.containers.forEach((container) => {
-                    containers.push(container);
-                });
-            }
-            else {
-                nodeSpec.set(pod.spec.nodeName, pod.spec.containers);
-            }
-        });
-        let nodeResourcesInUse = new Map();
-        nodeSpec.forEach((containers, node) => {
-            let cpuRequests = 0;
-            let memoryRequests = 0;
-            containers.forEach((container) => {
-                if (container.resources.requests != undefined) {
-                    let cpu = container.resources.requests.cpu;
-                    let memory = container.resources.requests.memory;
-                    if (cpu)
-                        cpuRequests += parse.parseUnitObj(cpu).val;
-                    if (memory)
-                        memoryRequests += parse.parseUnitObj(memory).val;
-                }
-            });
-            nodeResourcesInUse.set(node, { cpuRequests, memoryRequests });
-        });
-
         let resourcesStatus = new Map();
+        const groupedByNodeName = groupBy(pods.items, 'spec.nodeName');
+
         nodes.items.forEach((node) => {
             let nodeName = node.metadata.name;
-            let freeCpu = parse.parseUnitObj(node.status.allocatable.cpu).val - nodeResourcesInUse.get(nodeName).cpuRequests;
-            let freeMemory = parse.parseUnitObj(node.status.allocatable.memory).val - nodeResourcesInUse.get(nodeName).memoryRequests;
-            resourcesStatus.set(nodeName, { freeCpu, freeMemory })
+            let allocatableCpu = parse.parseUnitObj(node.status.allocatable.cpu).val;
+            let allocatableMemory = parse.parseUnitObj(node.status.allocatable.memory).val;
+            let pods = groupedByNodeName[nodeName];
+            let cpuRequests = 0;
+            let memoryRequests = 0;
+
+            pods.forEach(pod => {
+                pod.spec.containers.forEach(container => {
+                    if (container.resources.requests != undefined) {
+                        cpuRequests += this._getCpuInMiliCore(container.resources.requests.cpu);
+                        memoryRequests += this._getMemoryInKB(container.resources.requests.memory);
+                    }
+                });
+            });
+
+            let freeCpu = allocatableCpu - cpuRequests;
+            let freeMemory = allocatableMemory - memoryRequests;
+            resourcesStatus.set(nodeName, { allocatableCpu, allocatableMemory, cpuRequests, memoryRequests, freeCpu, freeMemory })
         });
         return resourcesStatus;
+    }
+
+    _getCpuInMiliCore(cpu) {
+        if (!cpu) return 0;
+        const res = parse.parseUnitObj(cpu);
+        switch (res.unit) {
+            case "m":
+                return res.val;
+            case "": // 0.1 CPU == 100m
+                return res.val * 1000;
+            default:
+                throw new Error(`${res.unit} unit not defined`);
+        }
+    }
+
+    _getMemoryInKB(memory) {
+        if (!memory) return 0;
+        const res = parse.parseUnitObj(memory);
+        switch (res.unit) {
+            case "Ki":
+                return res.val;
+            case "M":
+                return res.val * 1000;
+            case "Mi": // mili bytes  0.001Mi == (1/1000)Mi == 1024*1024*(1/1000)bytes = 1024*1024 milli-bytes == 1048576m
+                return res.val * 1024 * 1024 / 1000;
+            case "Gi":
+                return res.val * 1000 * 1000;
+            default:
+                throw (new Error(`${res.unit} unit not defined`));
+        }
     }
 }
 
--- core/resource-manager/lib/adapters/prometheus.js
+++ core/resource-manager/lib/adapters/prometheus.js
@@ -3,6 +3,7 @@ const Adapter = require('./Adapter');
 const log = require('@hkube/logger').GetLogFromContainer();
 const component = require('../../common/consts/componentNames').AlgorithmDb;
 const client = require('@hkube/prometheus-client');
+var median = require('median')
 
 class PrometheusAdapter extends Adapter {
 
@@ -13,19 +14,25 @@ class PrometheusAdapter extends Adapter {
 
     async getData() {
         let currentDate = (new Date).getTime();
-        let twentyMinutesBefore = new Date(currentDate - (20 * 60 * 1000));
+        let twentyMinutesBefore = new Date(currentDate - (6 * 60 * 60 * 1000));
 
         let res = await client.range({
-            query: 'histogram_quantile(0.5, sum(rate(algorithm_net_histogram_bucket[2m])) by (algorithmName, le))/1000',
+            query: 'histogram_quantile(0.5, sum(rate(algorithm_net_histogram_bucket[2m])) by (algorithmName, le)) / 1000',
             start: twentyMinutesBefore / 1000,
             end: currentDate / 1000,
-            step: 172
+            step: 60
         });
         let arr = [];
-        res.data.result.forEach(metric => {
-
+        res.data.result.forEach(algorithm => {
+            let algoRunTime = [];
+            algorithm.values.forEach(slice => {
+                if (slice[1] != 0) {
+                    algoRunTime.push(slice[1]);
+                }
+            })
+            arr.push({ algorithmName: algorithm.metric.algorithmName, runTime: median(algoRunTime) })
         });
-        // console.log(res)
+        return arr;
     }
 }
 
