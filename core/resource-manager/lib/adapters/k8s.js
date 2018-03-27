cc6bdc23a9e465177e626017109e31398e301291
Nassi Harel <nassi9@gmail.com>
2018-03-27 17:24:24 +0300

    Merge branch 'master' of github.com:kube-HPC/resource-manager

--- core/resource-manager/lib/adapters/k8s.js
+++ core/resource-manager/lib/adapters/k8s.js
@@ -16,33 +16,40 @@ class K8sAdapter extends Adapter {
     }
 
     async getData() {
-        let nodes = await this._client.nodes.get();
-        let pods = await this._client.pods.get();
-        let resourcesStatus = new Map();
-        const groupedByNodeName = groupBy(pods.items, 'spec.nodeName');
+        let data = this.cache.get();
+        if (data) {
+            return data;
+        }
+        else {
+            let nodes = await this._client.nodes.get();
+            let pods = await this._client.pods.get();
+            let resourcesStatus = new Map();
+            const groupedByNodeName = groupBy(pods.items, 'spec.nodeName');
 
-        nodes.items.forEach((node) => {
-            let nodeName = node.metadata.name;
-            let allocatableCpu = parse.parseUnitObj(node.status.allocatable.cpu).val;
-            let allocatableMemory = parse.parseUnitObj(node.status.allocatable.memory).val;
-            let pods = groupedByNodeName[nodeName];
-            let cpuRequests = 0;
-            let memoryRequests = 0;
+            nodes.items.forEach((node) => {
+                let nodeName = node.metadata.name;
+                let allocatableCpu = parse.parseUnitObj(node.status.allocatable.cpu).val;
+                let allocatableMemory = parse.parseUnitObj(node.status.allocatable.memory).val;
+                let pods = groupedByNodeName[nodeName];
+                let cpuRequests = 0;
+                let memoryRequests = 0;
 
-            pods.forEach(pod => {
-                pod.spec.containers.forEach(container => {
-                    if (container.resources.requests != undefined) {
-                        cpuRequests += this._getCpuInMiliCore(container.resources.requests.cpu);
-                        memoryRequests += this._getMemoryInKB(container.resources.requests.memory);
-                    }
+                pods.forEach(pod => {
+                    pod.spec.containers.forEach(container => {
+                        if (container.resources.requests != undefined) {
+                            cpuRequests += this._getCpuInMiliCore(container.resources.requests.cpu);
+                            memoryRequests += this._getMemoryInKB(container.resources.requests.memory);
+                        }
+                    });
                 });
-            });
 
-            let freeCpu = allocatableCpu - cpuRequests;
-            let freeMemory = allocatableMemory - memoryRequests;
-            resourcesStatus.set(nodeName, { allocatableCpu, allocatableMemory, cpuRequests, memoryRequests, freeCpu, freeMemory })
-        });
-        return resourcesStatus;
+                let freeCpu = allocatableCpu - cpuRequests;
+                let freeMemory = allocatableMemory - memoryRequests;
+                resourcesStatus.set(nodeName, { allocatableCpu, allocatableMemory, cpuRequests, memoryRequests, freeCpu, freeMemory })
+            });
+            this.cache.set(resourcesStatus);
+            return resourcesStatus;
+        }
     }
 
     _getCpuInMiliCore(cpu) {
--- core/resource-manager/lib/adapters/prometheus.js
+++ core/resource-manager/lib/adapters/prometheus.js
@@ -13,30 +13,37 @@ class PrometheusAdapter extends Adapter {
     }
 
     async getData() {
-        let currentDate = (new Date).getTime();
-        let twentyMinutesBefore = new Date(currentDate - (6 * 60 * 60 * 1000));
+        let data = this.cache.get();
+        if (data) {
+            return data;
+        }
+        else {
+            let currentDate = Date.now();
+            let sixHoursBefore = new Date(currentDate - (6 * 60 * 60 * 1000));
 
-        let res = await client.range({
-            query: 'algorithm_runtime_summary{quantile="0.5", algorithmName!~""}',
-            start: twentyMinutesBefore / 1000,
-            end: currentDate / 1000,
-            step: 60
-        });
-        let arr = [];
-        let algoRunTime = new Map();
-        res.data.result.forEach(algorithm => {
-            algorithm.values.forEach(slice => {
-                if (algoRunTime.has(algorithm.metric.algorithmName)) {
-                    let a = algoRunTime.get(algorithm.metric.algorithmName);
-                    a.push(parseFloat(slice[1]));
-                }
-                else {
-                    algoRunTime.set(algorithm.metric.algorithmName, [parseFloat(slice[1])]);
-                }
-            })
-        });
-        algoRunTime.forEach((val, key) => arr.push({ algorithmName: key, runTime: median(val) }));
-        return arr;
+            let res = await client.range({
+                query: 'algorithm_runtime_summary{quantile="0.5", algorithmName!~""}',
+                start: sixHoursBefore / 1000,
+                end: currentDate / 1000,
+                step: 60
+            });
+            let arr = [];
+            let algoRunTime = new Map();
+            res.data.result.forEach(algorithm => {
+                algorithm.values.forEach(slice => {
+                    if (algoRunTime.has(algorithm.metric.algorithmName)) {
+                        let a = algoRunTime.get(algorithm.metric.algorithmName);
+                        a.push(parseFloat(slice[1]));
+                    }
+                    else {
+                        algoRunTime.set(algorithm.metric.algorithmName, [parseFloat(slice[1])]);
+                    }
+                })
+            });
+            algoRunTime.forEach((val, key) => arr.push({ algorithmName: key, runTime: median(val) }));
+            this.cache.set(arr);
+            return arr;
+        }
     }
 }
 
