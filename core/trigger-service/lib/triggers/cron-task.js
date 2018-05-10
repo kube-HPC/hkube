a456b32ee421b7354d46741015d0cad43793fcf4
yehiyam <yehiyam@gmail.com>
2018-05-10 10:51:44 +0300

    check if there are any triggers

--- core/trigger-service/lib/triggers/cron-task.js
+++ core/trigger-service/lib/triggers/cron-task.js
@@ -1,5 +1,5 @@
-const {CronJob} = require('cron');
-const {componentName, storedPipelineEvents} = require('../consts/index');
+const { CronJob } = require('cron');
+const { componentName, storedPipelineEvents } = require('../consts/index');
 const log = require('@hkube/logger').GetLogFromContainer();
 const triggerQueue = require('../trigger-queue');
 const storedPipelineListener = require('../stored-pipelines-listener');
@@ -15,24 +15,26 @@ class CronTask {
         storedPipelineListener.on(storedPipelineEvents.prefix.CHANGE, t => this.addTrigger(t));
         storedPipelineListener.on(storedPipelineEvents.prefix.DELETE, t => this.removeTask(t.pipelineName));
         const triggers = await storedPipelineListener.getTriggeredPipelineByType(storedPipelineEvents.suffix.CRON);
-        triggers.forEach(t => this.addTrigger(t));
+        if (triggers) {
+            triggers.forEach(t => this.addTrigger(t));
+        }
     }
-    addTrigger(task, cb = () => {}) {
-        log.info(`new cron task with name ${task.name} added with cron ${task.triggers.cron} `, { component: componentName.CRON});
+    addTrigger(task, cb = () => { }) {
+        log.info(`new cron task with name ${task.name} added with cron ${task.triggers.cron} `, { component: componentName.CRON });
         const job = new CronJob(task.triggers.cron, () => {
-            triggerQueue.addTrigger({name: task.name, flowInput: [], jobId: 'cron'}, (err, res) => {
+            triggerQueue.addTrigger({ name: task.name, flowInput: [], jobId: 'cron' }, (err, res) => {
                 if (err) {
-                    log.error(`callback sent from trigger-queue with error  ${err}`, { component: componentName.CRON});
+                    log.error(`callback sent from trigger-queue with error  ${err}`, { component: componentName.CRON });
                 }
                 cb(task.name);
             });
-            log.info(`cron job with ${task.name} Is Executed according to schedule ${task.triggers.cron}`, { component: componentName.CRON});
+            log.info(`cron job with ${task.name} Is Executed according to schedule ${task.triggers.cron}`, { component: componentName.CRON });
         }, null, true);
-        
-        this.tasks.push({task, job});
+
+        this.tasks.push({ task, job });
     }
     removeTask(pipelineName) {
-        log.info(` task with name ${pipelineName} removed from cron `, { component: componentName.CRON});
+        log.info(` task with name ${pipelineName} removed from cron `, { component: componentName.CRON });
         this.tasks = this.tasks.filter(t => t.task.triggerPipeline !== pipelineName);
     }
 }
