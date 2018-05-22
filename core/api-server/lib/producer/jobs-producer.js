c2d1227b55dd20eb51b434ef8bdaf4daa7477415
Nassi Harel <nassi9@gmail.com>
2018-05-22 14:58:17 +0300

    fix jobID issue

--- core/api-server/lib/producer/jobs-producer.js
+++ core/api-server/lib/producer/jobs-producer.js
@@ -20,19 +20,19 @@ class JobProducer {
         setting.tracer = tracer;
         this._producer = new Producer({ setting });
         this._producer.on(Events.WAITING, (event) => {
-            log.info(`${Events.WAITING} ${event.options.data.jobID}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobID, status: States.WAITING });
+            log.info(`${Events.WAITING} ${event.options.data.jobId}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobId, status: States.WAITING });
         }).on(Events.ACTIVE, (event) => {
-            log.info(`${Events.ACTIVE} ${event.options.data.jobID}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobID, status: States.ACTIVE });
+            log.info(`${Events.ACTIVE} ${event.options.data.jobId}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobId, status: States.ACTIVE });
         }).on(Events.COMPLETED, (event) => {
-            log.info(`${Events.COMPLETED} ${event.options.data.jobID}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobID, status: States.COMPLETED });
+            log.info(`${Events.COMPLETED} ${event.options.data.jobId}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobId, status: States.COMPLETED });
         }).on(Events.FAILED, (event) => {
-            log.error(`${Events.FAILED} ${event.options.data.jobID}, ${event.error}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobID, status: States.FAILED });
+            log.error(`${Events.FAILED} ${event.options.data.jobId}, ${event.error}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobId, status: States.FAILED });
         }).on(Events.STALLED, (event) => {
-            log.error(`${Events.STALLED} ${event.options.data.jobID}, ${event.error}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobID, status: States.STALLED });
+            log.error(`${Events.STALLED} ${event.options.data.jobId}, ${event.error}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobId, status: States.STALLED });
         }).on(Events.CRASHED, async (event) => {
-            log.error(`${Events.CRASHED} ${event.options.data.jobID}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobID, status: States.FAILED });
-            const pipeline = await stateManager.getExecution({ jobId: event.options.data.jobID });
-            stateManager.setJobStatus({ jobId: event.options.data.jobID, pipeline: pipeline.name, status: States.FAILED, error: event.error, level: levels.error.name });
+            log.error(`${Events.CRASHED} ${event.options.data.jobId}`, { component: components.JOBS_PRODUCER, jobId: event.options.data.jobId, status: States.FAILED });
+            const pipeline = await stateManager.getExecution({ jobId: event.options.data.jobId });
+            stateManager.setJobStatus({ jobId: event.options.data.jobId, pipeline: pipeline.name, status: States.FAILED, error: event.error, level: levels.error.name });
         });
     }
 
