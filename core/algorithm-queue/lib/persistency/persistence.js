dc072f1300a6b01f3e902cc3631f93ae2fd2fc15
Nassi Harel <nassi9@gmail.com>
2018-05-28 09:01:15 +0300

    etcd client v4

--- core/algorithm-queue/lib/persistency/persistence.js
+++ core/algorithm-queue/lib/persistency/persistence.js
@@ -9,10 +9,10 @@ class Persistence {
         this.queueName = null;
         this.etcdConfig = null;
         this.etcd = new Etcd();
-    }  
-    init({options}) {
+    }
+    init({ options }) {
         this.options = options;
-        const {etcd, algorithmType, serviceName} = options;
+        const { etcd, algorithmType, serviceName } = options;
         this.queueName = algorithmType;
         //    this.queue = queue;
         this.etcd.init({ etcd, serviceName });
@@ -20,16 +20,16 @@ class Persistence {
     }
 
     async store(data) {
-        log.debug('storing data to etcd storage', { component: components.ETCD_PERSISTENT});
+        log.debug('storing data to etcd storage', { component: components.ETCD_PERSISTENT });
         const bullQueue = producerSingleton.get.getQueueByJobType(this.options.algorithmType);
         const pendingAmount = await bullQueue.getWaitingCount();
-        const status = await this.etcd.algorithms.algorithmQueue.setState({queueName: this.queueName, data, pendingAmount});
+        const status = await this.etcd.algorithms.algorithmQueue.set({ queueName: this.queueName, data, pendingAmount });
         if (status) {
-            log.debug('queue stored successfully', { component: components.ETCD_PERSISTENT});
+            log.debug('queue stored successfully', { component: components.ETCD_PERSISTENT });
         }
     }
     get() {
-        return this.etcd.algorithms.algorithmQueue.getState({queueName: this.queueName});
+        return this.etcd.algorithms.algorithmQueue.get({ queueName: this.queueName });
     }
 }
 
