47583e148ed0dad804251b655c280e1ada560890
Nassi Harel <nassi9@gmail.com>
2018-06-13 09:24:32 +0300

    update configs

--- core/algorithm-queue/config/logger/config.production.js
+++ core/algorithm-queue/config/logger/config.production.js
@@ -1,15 +1,9 @@
 const config = {};
 config.transport = {
     console: false,
+    fluentd: true,
     logstash: false,
-    fluentd: false,
     file: false
 };
-config.logstash = {
-    logstashURL: '127.0.0.1',
-    logstashPort: 28777
-};
-config.extraDetails = false;
 config.verbosityLevel = process.env.HKUBE_LOG_LEVEL || 2;
-config.isDefault = true;
 module.exports = config;
