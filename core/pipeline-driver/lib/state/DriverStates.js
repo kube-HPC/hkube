e5b0c77b46f2392d5639345e053a1177561f2f60
Nassi Harel <nassi9@gmail.com>
2018-07-18 10:28:09 +0300

    update hkube/etcd

---
+++ core/pipeline-driver/lib/state/DriverStates.js
@@ -0,0 +1,9 @@
+module.exports = {
+    IDLE: 'idle',
+    ACTIVE: 'active',
+    PAUSED: 'paused',
+    RESUMED: 'resumed',
+    FAILED: 'failed',
+    RECOVERING: 'recovering',
+    STOPPED: 'stopped'
+};
---
+++ core/pipeline-driver/lib/state/NodeStates.js
@@ -0,0 +1,12 @@
+module.exports = {
+    CREATING: 'creating',
+    PENDING: 'pending',
+    STALLED: 'stalled',
+    ACTIVE: 'active',
+    COMPLETED: 'completed',
+    SUCCEED: 'succeed',
+    FAILED: 'failed',
+    RECOVERING: 'recovering',
+    STOPPED: 'stopped',
+    STOP: 'stop'
+};
