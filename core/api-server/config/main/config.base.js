49ed7687644b9ca67a65a97e33d5d0b26ce3c88b
bahalool <bahalool@gmail.com>
2018-12-02 11:17:06 +0200

    get value from env - fs adapter BASE_FS_ADAPTER_DIRECTORY

--- core/api-server/config/main/config.base.js
+++ core/api-server/config/main/config.base.js
@@ -89,7 +89,7 @@ config.s3 = {
 };
 
 config.fs = {
-    baseDirectory: process.env.FS_DIRECTORY || '/var/tmp/fs/storage'
+    baseDirectory: process.env.BASE_FS_ADAPTER_DIRECTORY || '/var/tmp/fs/storage'
 };
 
 config.storageAdapters = {
