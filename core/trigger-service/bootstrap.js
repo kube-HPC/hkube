83b5aa2287b031773c9755b183f26eda4deb0a21
Nassi Harel <nassi9@gmail.com>
2019-03-20 09:14:56 +0200

    updates

--- core/trigger-service/bootstrap.js
+++ core/trigger-service/bootstrap.js
@@ -1,15 +1,15 @@
 const Logger = require('@hkube/logger');
 const configIt = require('@hkube/config');
 const { tracer } = require('@hkube/metrics');
-const monitor = require('@hkube/redis-utils').Monitor;
 const { componentName } = require('./lib/consts/index');
+const component = componentName.MAIN;
 let log;
 
 const modules = [
     './lib/store/store-manager',
     './lib/queue/trigger-runner',
     './lib/pipelines/stored-pipelines-listener',
-    './lib/pipelines/pipeline-producer',
+    './lib/pipelines/pipeline-producer'
 ];
 
 class Bootstrap {
@@ -20,19 +20,12 @@ class Bootstrap {
             this._handleErrors();
 
             log = new Logger(main.serviceName, logger);
-            log.info('running application in ' + configIt.env() + ' environment', { component: componentName.MAIN });
+            log.info(`running application with env: ${configIt.env()}, version: ${main.version}, node: ${process.versions.node}`, { component });
 
-            monitor.on('ready', (data) => {
-                log.info((data.message).green, { component: componentName.MAIN });
-            });
-            monitor.on('close', (data) => {
-                log.error(data.error.message, { component: componentName.MAIN });
-            });
-            monitor.check(main.redis);
             if (main.tracer) {
                 await tracer.init(main.tracer);
             }
-            await Promise.all(modules.map(m => require(m).init(main)));// eslint-disable-line global-require, import/no-dynamic-require
+            await Promise.all(modules.map(m => require(m).init(main)));
 
             config = main;
         }
@@ -45,33 +38,33 @@ class Bootstrap {
 
     _onInitFailed(error) {
         if (log) {
-            log.error(error.message, { component: componentName.MAIN }, error);
+            log.error(error.message, { component }, error);
             log.error(error);
         }
         else {
-            console.error(error.message);// eslint-disable-line 
-            console.error(error);// eslint-disable-line
+            console.error(error.message);
+            console.error(error);
         }
         process.exit(1);
     }
 
     _handleErrors() {
         process.on('exit', (code) => {
-            log.info('exit' + (code ? ' code ' + code : ''), { component: componentName.MAIN });
+            log.info('exit' + (code ? ' code ' + code : ''), { component });
         });
         process.on('SIGINT', () => {
-            log.info('SIGINT', { component: componentName.MAIN });
+            log.info('SIGINT', { component });
             process.exit(1);
         });
         process.on('SIGTERM', () => {
-            log.info('SIGTERM', { component: componentName.MAIN });
+            log.info('SIGTERM', { component });
             process.exit(1);
         });
         process.on('unhandledRejection', (error) => {
-            log.error('unhandledRejection: ' + error, { component: componentName.MAIN }, error);
+            log.error('unhandledRejection: ' + error, { component }, error);
         });
         process.on('uncaughtException', (error) => {
-            log.error('uncaughtException: ' + error.message, { component: componentName.MAIN }, error);
+            log.error('uncaughtException: ' + error.message, { component }, error);
             log.error(error);
             process.exit(1);
         });
