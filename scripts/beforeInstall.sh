1c64e4bef3ef014a0fe4a041683f3c23a0cee456
Yehiyam Livneh <yehiyam@gmail.com>
2019-06-04 12:35:23 +0300

    add CI

---
+++ scripts/beforeInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ev
+docker run -d --name etcd -p 2380:2380 -p 4001:4001 quay.io/coreos/etcd:latest /usr/local/bin/etcd \
+  --data-dir=data.etcd --name "my-etcd" --cors='*' --initial-advertise-peer-urls http://0.0.0.0:2380 \
+  --listen-peer-urls http://0.0.0.0:2380 --advertise-client-urls http://0.0.0.0:4001 \
+  --listen-client-urls http://0.0.0.0:4001     --initial-cluster-state new
+docker run -d -p 9000:9000 --name minio1 -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" \
+  -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" minio/minio server /data
