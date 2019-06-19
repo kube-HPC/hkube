3ffc789f2d626b4446cafd2ca6e1282ceb4cddd8
Yehiyam Livneh <yehiyam@gmail.com>
2019-06-19 18:06:34 +0300

    fix formatter

--- core/task-executor/lib/helpers/formatters.js
+++ core/task-executor/lib/helpers/formatters.js
@@ -1,16 +1,13 @@
 module.exports = {
-    formatPipelineName(pipelineName) {
-        if (pipelineName.startsWith('raw-')) {
-            return 'raw';
-        }
-        return pipelineName;
-    },
     parseBool(value, defaultValue) {
         if (typeof value === 'boolean') {
             return value;
         }
-        if (typeof value === 'string' && value.toLowerCase() === 'true') {
-            return true;
+        if (typeof value === 'string') {
+            if (value.toLowerCase() === 'true') {
+                return true;
+            }
+            return false;
         }
         return defaultValue || false;
     },
