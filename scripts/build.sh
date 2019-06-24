dde2e6ccdeb3e243ed5c8cda2424ca8376a3b04b
Yehiyam Livneh <yehiyam@gmail.com>
2019-06-24 16:47:31 +0300

    debug CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 set -ev
 if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo ${DOCKER_HUB_PASS} | docker login --username yehiyam --password-stdin
+  echo ${CHANGED}
   for REPO in ${CHANGED}
   do
     echo ${REPO} changed. Running build
