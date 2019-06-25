50d187ce244797eeb94bc9f9930c015f9fc6d5b9
yehiyam <yehiyam@users.noreply.github.com>
2019-06-25 17:26:32 +0300

    fix volume mounts (#390)

--- core/algorithm-operator/lib/jobs/algorithm-builds.js
+++ core/algorithm-operator/lib/jobs/algorithm-builds.js
@@ -1,7 +1,7 @@
 const objectPath = require('object-path');
 const clonedeep = require('lodash.clonedeep');
 const log = require('@hkube/logger').GetLogFromContainer();
-const { applyEnvToContainer, applyStorage, applyVolumeMounts, applyVolumes, applyPrivileged } = require('@hkube/kubernetes-client').utils;
+const { applyEnvToContainer, applyStorage, applyVolumeMounts: applyVolumeMount, applyVolumes: applyVolume, applyPrivileged } = require('@hkube/kubernetes-client').utils;
 const { applyImage } = require('../helpers/kubernetes-utils');
 const components = require('../consts/componentNames');
 const { ALGORITHM_BUILDS } = require('../consts/containers');
@@ -28,6 +28,21 @@ const applyKanikoContainer = (inputSpec) => {
     return spec;
 };
 
+const applyVolumes = (inputSpec, volumes) => {
+    let spec = clonedeep(inputSpec);
+    volumes.forEach((volume) => {
+        spec = applyVolume(spec, volume);
+    });
+    return spec;
+};
+
+const applyVolumeMounts = (inputSpec, containerName, mounts) => {
+    let spec = clonedeep(inputSpec);
+    mounts.forEach((mount) => {
+        spec = applyVolumeMount(spec, containerName, mount);
+    });
+    return spec;
+};
 const createBuildJobSpec = ({ buildId, versions, registry, options }) => {
     if (!buildId) {
         const msg = 'Unable to create job spec. buildId is required';
