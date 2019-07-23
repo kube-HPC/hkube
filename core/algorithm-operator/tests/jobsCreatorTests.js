53e9677a81b7ff894f3235db1c671e57a79d9ef9
yehiyam <yehiyam@users.noreply.github.com>
2019-07-23 09:23:02 +0300

    fix unit tests (#435)
    * fix unit tests

--- core/algorithm-operator/tests/jobsCreatorTests.js
+++ core/algorithm-operator/tests/jobsCreatorTests.js
@@ -17,7 +17,16 @@ describe('jobCreator', () => {
     });
     it('should apply all required properties', () => {
         const buildId = 'my-alg-12345'
-        const res = createBuildJobSpec({ buildId, versions: { versions: [{ project: 'algorithm-builder', tag: 'v1.2' }] }, options: main });
+        const res = createBuildJobSpec({
+            buildId, versions: { versions: [{ project: 'algorithm-builder', tag: 'v1.2' }] }, secret: {
+                metadata: {
+                    name: 'test'
+                },
+                data: {
+                    
+                }
+            }, options: main
+        });
         expect(res).to.nested.include({ 'metadata.name': 'build-' + buildId });
         expect(res).to.nested.include({ 'spec.template.spec.containers[0].image': 'hkube/algorithm-builder:v1.2' });
         expect(res).to.nested.include({ 'metadata.labels.build-id': buildId });
@@ -38,6 +47,13 @@ describe('jobCreator', () => {
                     {
                         project: 'kaniko', tag: 'v1.1.0'
                     }]
+            }, secret: {
+                metadata: {
+                    name: 'test'
+                },
+                data: {
+                    
+                }
             },
             options
         });
@@ -66,6 +82,13 @@ describe('jobCreator', () => {
             options,
             registry: {
                 registry: 'my-reg:5000'
+            }, secret: {
+                metadata: {
+                    name: 'test'
+                },
+                data: {
+                    
+                }
             }
         });
         expect(res.spec.template.spec.containers).to.be.of.length(2);
@@ -80,7 +103,14 @@ describe('jobCreator', () => {
             ...main,
             buildMode: 'docker'
         }
-        const res = createBuildJobSpec({ buildId, versions: { versions: [{ project: 'algorithm-builder', tag: 'v1.2' }] }, options });
+        const res = createBuildJobSpec({ buildId, versions: { versions: [{ project: 'algorithm-builder', tag: 'v1.2' }] }, secret: {
+            metadata: {
+                name: 'test'
+            },
+            data: {
+                
+            }
+        }, options });
         expect(res.spec.template.spec.containers).to.be.of.length(1);
         expect(res.spec.template.spec.containers[0].securityContext.privileged).to.be.true;
     });
