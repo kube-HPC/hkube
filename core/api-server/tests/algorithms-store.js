141a3b2850c1928c80c80610c3cfd82b01d7b623
maty21 <maty21@gmail.com>
2019-10-07 15:03:43 +0300

    fix webhook

--- core/api-server/tests/algorithms-store.js
+++ core/api-server/tests/algorithms-store.js
@@ -609,7 +609,7 @@ describe('Store/Algorithms', () => {
                     mem: "6000Mi",
                     cpu: 1,
                     gitRepository: {
-                        url: "https://github.com/maty21/statistisc",
+                        url: "https://github.com/maty21/statistisc.git",
                         branchName: "master",
                         gitKind: "github"
                     },
