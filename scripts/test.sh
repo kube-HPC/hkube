0661bf044a8dd4a4e4bb17441f30c2b7dfc9cd30
Yehiyam Livneh <yehiyam@gmail.com>
2019-11-04 16:10:53 +0200

    test with error

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,7 @@ for REPO in ${CHANGED}
 do
   echo ${REPO} changed. Running tests
   lerna run --scope $REPO lint
+  echo lint for $REPO returned $?
   lerna run --scope $REPO test
+  echo test for $REPO returned $?
 done
