b557a075b5925155ae20dc4682cfa0afc65d778e
Cube <maty21@gmail.com>
2019-11-27 11:55:29 +0200

    Add gitlab (#577)
    * start
    
    * add gitlab support
    
    * add mock
    
    * Merge branch 'master' into add-gitlab
    
    * fix code review
    
    * Merge branch 'add-gitlab' of https://github.com/kube-HPC/hkube into add-gitlab
    
    * Merge branch 'master' into add-gitlab
    
    * fix gitlab host name
    
    * Merge branch 'add-gitlab' of https://github.com/kube-HPC/hkube into add-gitlab

---
+++ core/api-server/tests/mocks/gitlab-sample.js
@@ -0,0 +1,61 @@
+module.exports = {
+    object_kind: 'push',
+    event_name: 'push',
+    before: 'ec89c9e11327872ffe649bdc1353c4a591a6f434',
+    after: '4634baa4a363b27421f29182694c9e9f5bc9ef38',
+    ref: 'refs/heads/master',
+    checkout_sha: '4634baa4a363b27421f29182694c9e9f5bc9ef38',
+    message: null,
+    user_id: 404390,
+    user_name: 'maty zisserman',
+    user_username: 'maty21',
+    user_email: '',
+    user_avatar: 'https://secure.gravatar.com/avatar/b03f2e50064151dcfe6fcbd70fad0e8f?s=80&d=identicon',
+    project_id: 8077802,
+    project: {
+        id: 8077802,
+        name: 'anomaly',
+        description: '',
+        web_url: 'https://gitlab.com/maty21/anomaly',
+        avatar_url: null,
+        git_ssh_url: 'git@gitlab.com:maty21/anomaly.git',
+        git_http_url: 'https://gitlab.com/maty21/anomaly.git',
+        namespace: 'maty zisserman',
+        visibility_level: 20,
+        path_with_namespace: 'maty21/anomaly',
+        default_branch: 'master',
+        ci_config_path: null,
+        homepage: 'https://gitlab.com/maty21/anomaly',
+        url: 'git@gitlab.com:maty21/anomaly.git',
+        ssh_url: 'git@gitlab.com:maty21/anomaly.git',
+        http_url: 'https://gitlab.com/maty21/anomaly.git'
+    },
+    commits: [
+        {
+            id: '4634baa4a363b27421f29182694c9e9f5bc9ef38',
+            message: 'Update README.md',
+            timestamp: '2019-11-17T13:23:56Z',
+            url: 'https://gitlab.com/maty21/anomaly/commit/4634baa4a363b27421f29182694c9e9f5bc9ef38',
+            author: {
+                name: 'maty zisserman',
+                email: 'maty21@gmail.com'
+            },
+            added: [],
+            modified: [
+                'README.md'
+            ],
+            removed: []
+        }
+    ],
+    total_commits_count: 1,
+    push_options: {},
+    repository: {
+        name: 'anomaly',
+        url: 'git@gitlab.com:maty21/anomaly.git',
+        description: '',
+        homepage: 'https://gitlab.com/maty21/anomaly',
+        git_http_url: 'https://gitlab.com/maty21/anomaly.git',
+        git_ssh_url: 'git@gitlab.com:maty21/anomaly.git',
+        visibility_level: 20
+    }
+}
