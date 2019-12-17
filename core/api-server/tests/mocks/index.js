ac7a04a8fb8728b42f521a5471ff9b6de27926c3
Nassi Harel <nassi9@gmail.com>
2019-12-17 10:09:28 +0200

    fix: webhook tests

--- core/api-server/tests/mocks/index.js
+++ core/api-server/tests/mocks/index.js
@@ -1,13 +1,11 @@
 const algorithms = require('./algorithms.json');
 const pipelines = require('./pipelines.json');
 const triggersTree = require('./triggers-tree.json');
-const webhookStub = require('./webhook');
 const workerStub = require('./worker');
 
 module.exports = {
     algorithms,
     pipelines,
     triggersTree,
-    webhookStub,
     workerStub
 };
