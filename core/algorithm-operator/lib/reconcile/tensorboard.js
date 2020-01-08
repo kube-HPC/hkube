fa3231aa6d9acf26623fc21ad0962f4b3595ef49
yehiyam <yehiyam@users.noreply.github.com>
2020-01-08 16:07:47 +0200

    Update tensorboard.js

--- core/algorithm-operator/lib/reconcile/tensorboard.js
+++ core/algorithm-operator/lib/reconcile/tensorboard.js
@@ -29,8 +29,7 @@ const updateTensorboards = async () => {
     const boards = await etcd.getTensorboards();
     const creating = boards.filter(b => b.status === boardStatuses.CREATING);
     await Promise.all(creating.forEach(async (board) => {
-        // const url = `http://board-service-${board.boardId}.default.svc`;
-        const url = `http://40.69.222.75/hkube/board/${board.boardId}`;
+        const url = `http://board-service-${board.boardId}.default.svc`;
         try {
             const result = await rp({ uri: url, resolveWithFullResponse: true });
             // eslint-disable-next-line no-param-reassign
