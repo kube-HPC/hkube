2e6feb759276ee7d1f74e7c05088426c8addcae0
yehiyam <yehiyam@users.noreply.github.com>
2020-03-17 11:13:28 +0200

    change build parameters to envs (#768)
    * change build parameters to envs
    
    * cleanup

--- core/algorithm-builder/lib/builds/build-utils.sh
+++ core/algorithm-builder/lib/builds/build-utils.sh
@@ -17,32 +17,39 @@ dockerLogin() {
   fi
 }
 
+printEnvs(){
+  echo
+  echo IMAGE_NAME=${IMAGE_NAME}
+  echo BUILD_PATH=${BUILD_PATH}
+  echo BASE_IMAGE=${BASE_IMAGE}
+  echo DOCKER_PULL_REGISTRY=${DOCKER_PULL_REGISTRY}
+  echo INSECURE_PULL=${INSECURE_PULL:-"false"}
+  echo SKIP_TLS_VERIFY_PULL=${SKIP_TLS_VERIFY_PULL:-"false"}
+  echo DOCKER_PUSH_REGISTRY=${DOCKER_PUSH_REGISTRY}
+  echo SKIP_TLS_VERIFY=${SKIP_TLS_VERIFY:-"false"}
+  echo INSECURE=${INSECURE:-"false"}
+  echo PACKAGES_REGISTRY=${PACKAGES_REGISTRY}
+  echo REMOVE_IMAGE=${REMOVE_IMAGE}
+  echo TMP_FOLDER=${TMP_FOLDER}
+  echo
+}
+
 dockerBuildOpenshift() {
-  image=$1
-  buildPath=$2
-  workspace=${3:-/workspace}
-  commands=${4:-/commands}
-  export baseImage=$5
-  export packagesRegistry=$6
-  export packagesToken=$7
-  insecure=${8}
-  insecure_pull=${9}
-  skip_tls_verify=${10}
-  skip_tls_verify_pull=${11}
+  export image=${IMAGE_NAME}
+  export buildPath=${BUILD_PATH}
+  export workspace="${TMP_FOLDER}/workspace"
+  export commands="${TMP_FOLDER}/commands"
+  export baseImage=${BASE_IMAGE}
+  export packagesRegistry=${PACKAGES_REGISTRY}
+  export packagesToken=${PACKAGES_TOKEN}
 
   echo "Building image ${image}"
   echo copy context from ${buildPath} to ${workspace}
   cp -r ${buildPath}/* ${workspace}
-  # echo copy docker creds
-  # cp ~/.docker/config.json ${commands}/
   
   envsubst < ${workspace}/dockerfile/DockerfileTemplate > ${workspace}/dockerfile/Dockerfile
   sed -i '/^ARG /d' ${workspace}/dockerfile/Dockerfile
-  options=""
-  if [[ $insecure == true ]]; then options="${options} --insecure"; fi
-  if [[ $insecure_pull == true ]]; then options="${options} --insecure-pull"; fi
-  if [[ $skip_tls_verify == true ]]; then options="${options} --skip-tls-verify"; fi
-  if [[ $skip_tls_verify_pull == true ]]; then options="${options} --skip-tls-verify-pull"; fi
+
   echo BUILD_ID=${BUILD_ID}
   echo '#!/bin/bash' > ${commands}/run
   echo 'set -o pipefail' >> ${commands}/run
@@ -53,9 +60,7 @@ dockerBuildOpenshift() {
   echo "oc apply -f /commands/buildConfig.yaml" >> ${commands}/run
   echo "oc apply -f /commands/dockerCredsSecret.yaml" >> ${commands}/run
   echo "oc secrets link builder build-registry-secret" >> ${commands}/run
-  # echo "oc apply -f /commands/imageStream.yaml" >> ${commands}/run
   echo "oc start-build ${BUILD_ID} --from-dir /workspace/ --follow --wait" >>${commands}/run
-  # echo "oc delete -f /commands/buildConfig.yaml" >> ${commands}/run
 
   chmod +x ${commands}/run
   touch ${commands}/output
@@ -75,23 +80,21 @@ dockerBuildOpenshift() {
 }
 
 dockerBuildKaniko() {
-  image=$1
-  buildPath=$2
-  workspace=${3:-/workspace}
-  commands=${4:-/commands}
-  baseImage=$5
-  packagesRegistry=$6
-  packagesToken=$7
-  insecure=${8}
-  insecure_pull=${9}
-  skip_tls_verify=${10}
-  skip_tls_verify_pull=${11}
+  image=${IMAGE_NAME}
+  buildPath=${BUILD_PATH}
+  workspace="${TMP_FOLDER}/workspace"
+  commands="${TMP_FOLDER}/commands"
+  baseImage=${BASE_IMAGE}
+  packagesRegistry=${PACKAGES_REGISTRY}
+  packagesToken=${PACKAGES_TOKEN}
+  insecure=${INSECURE}
+  insecure_pull=${INSECURE_PULL}
+  skip_tls_verify=${SKIP_TLS_VERIFY}
+  skip_tls_verify_pull=${SKIP_TLS_VERIFY_PULL}
 
   echo "Building image ${image}"
   echo copy context from ${buildPath} to ${workspace}
   cp -r ${buildPath}/* ${workspace}
-  # echo copy docker creds
-  # cp ~/.docker/config.json ${commands}/
   options=""
   if [[ $insecure == true ]]; then options="${options} --insecure"; fi
   if [[ $insecure_pull == true ]]; then options="${options} --insecure-pull"; fi
