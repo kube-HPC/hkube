5d0a67484f7db0f7bd097c1e0e26683d681557d5
Yehiyam Livneh <yehiyam@gmail.com>
2020-05-14 16:41:35 +0300

    updates

--- scripts/build-branch.sh
+++ scripts/build-branch.sh
@@ -8,7 +8,7 @@
 set -eo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export OUT_VALUES_DIR=${DIR}/../deployment/
-export REPOS=$(lerna list)
+export REPOS=${REPOS:-$(lerna list)}
 export TRAVIS_PULL_REQUEST=true
 export TRAVIS_PULL_REQUEST_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export PREV_BUILD_ID=$(cat ${OUT_VALUES_DIR}BUILD_ID)
@@ -16,8 +16,8 @@ export PREV_BUILD_ID=${PREV_BUILD_ID:-"0"}
 echo PREV_BUILD_ID=$PREV_BUILD_ID
 export TRAVIS_JOB_NUMBER=${BUILD_ID:-$((PREV_BUILD_ID+1))}
 
-export OUT_VALUES=${OUT_VALUES_DIR}values-${TRAVIS_PULL_REQUEST_BRANCH}.yaml
-echo "" > ${OUT_VALUES}
+export OUT_VALUES=${OUT_VALUES:-${HOME}/dev/hkube/helm/hkube/values.yaml}
+touch ${OUT_VALUES}
 echo ${TRAVIS_JOB_NUMBER}>${OUT_VALUES_DIR}/BUILD_ID
 echo "building branch ${TRAVIS_PULL_REQUEST_BRANCH} with id ${TRAVIS_JOB_NUMBER}"
 for REPO in ${REPOS}
@@ -25,9 +25,9 @@ do
     VERSION=v$(jq -r .version ./core/${REPO}/package.json)
     VERSION=${VERSION}-${TRAVIS_PULL_REQUEST_BRANCH}-${TRAVIS_JOB_NUMBER}
     echo building ${REPO}:${VERSION}
-    export PRIVATE_REGISTRY=docker.io/hkube
-    lerna run --scope $REPO build --stream
-    yq n $(echo ${REPO}|tr '-' '_').image.tag ${VERSION} >> ${OUT_VALUES}
+    # export PRIVATE_REGISTRY=docker.io/hkube
+    # lerna run --scope $REPO build --stream
+    yq w -i ${OUT_VALUES} $(echo ${REPO}|tr '-' '_').image.tag ${VERSION} 
 
     echo "build done for ${REPO}"
 done
