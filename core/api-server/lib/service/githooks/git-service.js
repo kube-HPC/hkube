84ee5f55212c563ffb0599c9d0fa08362f693710
Nassi Harel <nassi9@gmail.com>
2020-06-21 17:30:11 +0300

    fix: gitlab build, commit id issue (#827)
    Co-authored-by: yehiyam <yehiyam@users.noreply.github.com>

--- core/api-server/lib/service/githooks/git-service.js
+++ core/api-server/lib/service/githooks/git-service.js
@@ -4,55 +4,55 @@ const urlLib = require('url');
 const { InvalidDataError } = require('../../errors');
 
 class GitService {
-    async getGithubCommit({ url, commitId, tag, branchName, token }) {
-        let lastCommit;
-        const { owner, repo } = this._parseGitUrl(url);
+    async getGithubCommit({ webUrl, commit, tag, branchName, token }) {
+        let gitCommit;
+        const { owner, repo } = this._parseGitUrl(webUrl);
         const params = {
             owner,
             repo,
-            sha: commitId || tag || branchName,
+            sha: (commit && commit.id) || tag || branchName,
             per_page: 1,
             page: 1
         };
         try {
             const octokit = new Octokit({ auth: token });
-            lastCommit = await octokit.repos.listCommits(params);
+            const listCommits = await octokit.repos.listCommits(params);
+            const data = listCommits.data[0];
+            gitCommit = {
+                id: data.sha,
+                timestamp: data.commit.committer.date,
+                message: data.commit.message
+            };
         }
         catch (error) {
-            throw new InvalidDataError(`${error.message} (${url})`);
+            throw new InvalidDataError(`${error.message} (${webUrl})`);
         }
-        const data = lastCommit.data[0];
-        const commit = {
-            id: data.sha,
-            timestamp: data.commit.committer.date,
-            message: data.commit.message
-        };
-        return commit;
+        return gitCommit;
     }
 
-    async getGitlabCommit({ url, commitId, tag, branchName, token }) {
-        let lastCommit;
-        const { host, owner, repo } = this._parseGitUrl(url);
+    async getGitlabCommit({ webUrl, commit, tag, branchName, token }) {
+        let gitCommit;
+        const { host, owner, repo } = this._parseGitUrl(webUrl);
         const params = {
             perPage: 1,
             maxPages: 1,
             showPagination: false,
-            ref_name: commitId || tag || branchName
+            ref_name: (commit && commit.id) || tag || branchName
         };
         try {
             const services = new ProjectsBundle({ host, token });
-            lastCommit = await services.Commits.all(`${owner}/${repo}`, params);
+            const listCommits = await services.Commits.all(`${owner}/${repo}`, params);
+            const data = listCommits[0];
+            gitCommit = {
+                id: data.id,
+                timestamp: data.committed_date,
+                message: data.message
+            };
         }
         catch (error) {
-            throw new InvalidDataError(`${error.message} (${url})`);
+            throw new InvalidDataError(`${error.message} (${webUrl})`);
         }
-        const data = lastCommit.data[0];
-        const commit = {
-            id: data.id,
-            timestamp: data.committed_date,
-            message: data.message
-        };
-        return commit;
+        return gitCommit;
     }
 
     _parseGitUrl(url) {
