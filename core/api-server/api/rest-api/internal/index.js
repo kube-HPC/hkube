ef49f4c9b7a227adf9ef50c7bc830387092ea6f0
Nassi Harel <nassi9@gmail.com>
2020-06-23 12:11:39 +0300

    feat: pipelines-triggers-tree (#837)
    * feat: pipelines-triggers-tree
    
    * Merge branch 'master' into pipelines-triggers-tree
    
    * feat: pipelines-triggers-tree
    
    * Merge branch 'pipelines-triggers-tree' of github.com:kube-HPC/hkube into pipelines-triggers-tree
    
    * feat: pipelines-triggers-tree
    
    * feat: pipelines-triggers-tree
    
    * feat: pipelines-triggers-tree

--- core/api-server/api/rest-api/internal/index.js
+++ core/api-server/api/rest-api/internal/index.js
@@ -1,4 +1,3 @@
-
 const execRouter = require('./exec');
 const algoRouter = require('./algorithms');
 
--- core/api-server/lib/service/caching.js
+++ core/api-server/lib/service/caching.js
@@ -1,4 +1,3 @@
-
 const request = require('request-promise');
 const querystring = require('querystring');
 
