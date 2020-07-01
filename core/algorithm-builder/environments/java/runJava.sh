57b46f51aa6989b83db04bda598d66c3c9cf8912
golanha <golanha@rnd-hub.com>
2020-07-01 07:01:35 +0300

    Control minimum memory at java startup

--- core/algorithm-builder/environments/java/runJava.sh
+++ core/algorithm-builder/environments/java/runJava.sh
@@ -3,4 +3,11 @@ if [ -z "${JAVA_MAX_MEM}" ]; then
 else
   MAX_MEM="${JAVA_MAX_MEM}"
 fi
-java -Xmx${MAX_MEM} -jar wrapper.jar algorithm_unique_folder/encapsulated-algorithm.jar  2>&1 |tee /hkube-logs/stdout.log
+if [ -z "${JAVA_MIN_MEM}" ]; then
+  MIN_MEM="256M"
+else
+  MIN_MEM="${JAVA_MIN_MEM}"
+fi
+
+
+java -Xms${MIN_MEM} -Xmx${MAX_MEM} -jar wrapper.jar algorithm_unique_folder/encapsulated-algorithm.jar  2>&1 |tee /hkube-logs/stdout.log
