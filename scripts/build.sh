be89a3291428514bf0d420796c8874532a559954
Yehiyam Livneh <yehiyam@gmail.com>
2020-07-16 16:56:37 +0300

    debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -x
 if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo ${DOCKER_HUB_PASS} | docker login --username yehiyam --password-stdin
   echo ${CHANGED}
@@ -8,6 +8,7 @@ if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PU
     echo ${REPO} changed. Running build
     export PRIVATE_REGISTRY=docker.io/hkube
     lerna run --scope $REPO build
+    echo lerna run --scope $REPO build exited with code $?
     echo "build done for ${REPO}"
   done
 else
