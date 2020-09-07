f56273b7876860833081746ac759bf0df1c5e8fc
Nassi Harel <nassi9@gmail.com>
2020-09-07 13:32:52 +0300

    feat: improve cache-pipeline

---
+++ core/pipeline-driver/lib/tasks/cache-pipeline.js
@@ -0,0 +1,53 @@
+const { parser } = require('@hkube/parsers');
+const graphStore = require('../datastore/graph-store');
+
+class Cache {
+    async _checkCachePipeline(nodes) {
+        const node = nodes.find(n => n.cacheJobId);
+        if (node) {
+            await this._getResultFromPredecessors(node);
+        }
+    }
+
+    async _getResultFromPredecessors(node) {
+        const jobId = node.cacheJobId;
+        const graph = await graphStore.getGraph({ jobId });
+        const nodes = this._splitInputToNodes(node.input);
+        const parentOutput = [];
+
+        nodes.forEach((n) => {
+            const data = this._getNodeResult(graph, n.nodeName);
+            if (data) {
+                data.type = n.type;
+                parentOutput.push(data);
+            }
+        });
+        node.parentOutput = parentOutput.length === 0 ? null : parentOutput;
+    }
+
+    _splitInputToNodes(input) {
+        const newNodes = new Map();
+        input.forEach((i) => {
+            const nodesNames = parser.extractNodesFromInput(i);
+            nodesNames.forEach(n => {
+                newNodes.set(n.nodeName, n);
+            });
+        });
+        return newNodes;
+    }
+
+    _getNodeResult(graph, nodeName) {
+        let result;
+        const node = graph.nodes.find(n => n.nodeName === nodeName);
+
+        if (node.batch && node.batch.length > 0) {
+            result = { node: nodeName, result: node.batch.map(b => b.output) };
+        }
+        else {
+            result = { node: nodeName, result: node.output };
+        }
+        return result;
+    }
+}
+
+module.exports = new Cache();
