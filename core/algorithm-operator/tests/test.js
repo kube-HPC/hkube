34034d018a03c0e6c1c43ea6be35ae7c1d8f3796
Yehiyam Livneh <yehiyam@gmail.com>
2020-10-08 15:42:27 +0300

    add warning for algorithm-queue not created

--- core/algorithm-operator/tests/test.js
+++ core/algorithm-operator/tests/test.js
@@ -21,6 +21,9 @@ describe('bootstrap', () => {
         mockery.disable();
         decache('../bootstrap');
     });
+    beforeEach(async () => {
+        await etcd._etcd._client.delete('/algorithms/store', { isPrefix: true })
+    });
     it('should init without error', async () => {
 
     });
@@ -28,5 +31,21 @@ describe('bootstrap', () => {
         await Promise.all(templateStoreStub.map(a => etcd._etcd.algorithms.store.set(a)));
         const template = await etcd.getAlgorithmTemplate({ name: 'algo2' });
         expect(template).to.eql(templateStoreStub[1]);
+    });
+
+    it('should list template store', async () => {
+        await Promise.all(templateStoreStub.map(a => etcd._etcd.algorithms.store.set(a)));
+        const { algorithms, count } = await etcd.getAlgorithmTemplates();
+        expect(algorithms).to.deep.include(templateStoreStub[1]);
+        expect(algorithms.length).to.eql(count);
+    });
+    it('should list template store above limit', async () => {
+
+        await Promise.all([...Array(110).keys()].map(i => etcd._etcd.algorithms.store.set({
+            name: `key-${i}`,
+        })));
+        const { algorithms, count } = await etcd.getAlgorithmTemplates();
+        expect(algorithms).to.deep.include({ name: 'key-99' });
+        expect(algorithms.length).to.not.eql(count);
     });
 });
