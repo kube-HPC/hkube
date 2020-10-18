e2e56c8dd39c0d56ccb5d858905c36ded4dd6ef3
golanha <golanha@rnd-hub.com>
2020-10-18 13:03:53 +0300

    apt-upadate before download

--- core/algorithm-builder/dockerfile/get-deps-java.sh
+++ core/algorithm-builder/dockerfile/get-deps-java.sh
@@ -8,4 +8,4 @@ export javaWrapperVersion='1.0-SNAPSHOT'
 docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=io.hkube:java-algo-parent:$javaWrapperVersion:pom -DremoteRepositories=https://oss.sonatype.org/content/repositories/snapshots -Ddest=/jars/java-algo-parent.xml
 mkdir -p $SCRIPTPATH/../environments/java/debs
 cd $SCRIPTPATH/../environments/java/debs
-apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests  --no-conflicts --no-breaks --no-replaces --no-enhances  --no-pre-depends libzmq-java | grep "^\w")
+apt-get update && apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests  --no-conflicts --no-breaks --no-replaces --no-enhances  --no-pre-depends libzmq-java | grep "^\w")
