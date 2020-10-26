5ee5831543b1a8c193a0980365cdf9988fff6140
golanha <golanha@rnd-hub.com>
2020-10-26 16:03:44 +0200

    Install dependencies with the version we use

--- core/algorithm-builder/dockerfile/get-deps-java.sh
+++ core/algorithm-builder/dockerfile/get-deps-java.sh
@@ -8,6 +8,10 @@ docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dep
 docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=io.hkube:interfaces:$javaWrapperVersion:jar -DremoteRepositories=https://oss.sonatype.org/content/repositories/snapshots -Ddest=/jars/interfaces.jar
 docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=io.hkube:java-algo-parent:$javaWrapperVersion:pom -DremoteRepositories=https://oss.sonatype.org/content/repositories/snapshots -Ddest=/jars/java-algo-parent.xml
 docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=io.hkube:algorithm-deployment-descriptor:$javaWrapperVersion:jar -DremoteRepositories=https://oss.sonatype.org/content/repositories/snapshots -Ddest=/jars/algorithm-deployment-descriptor.jar
+docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=org.apache.maven.plugins:maven-assembly-plugin:2.5.3:jar -Ddest=/jars/maven-assembly-plugin.jar
+docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=org.json:json:20190722:jar -Ddest=/jars/json.jar
+
+
 mkdir -p $SCRIPTPATH/../environments/java/debs
 cd $SCRIPTPATH/../environments/java/debs
 docker run --rm --workdir /tmp/pkg -v $SCRIPTPATH/../environments/java/debs:/tmp/pkg\
