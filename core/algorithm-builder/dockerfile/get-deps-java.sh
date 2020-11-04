63e4db4536d32fd1df3c8a46139b0becb613e587
golanha <golanha@rnd-hub.com>
2020-11-04 09:05:24 +0200

    1.3.02

--- core/algorithm-builder/dockerfile/get-deps-java.sh
+++ core/algorithm-builder/dockerfile/get-deps-java.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p $SCRIPTPATH/../environments/java/jars
 mkdir -p $SCRIPTPATH/../environments/java/m2
-export javaWrapperVersion='1.3.01'
+export javaWrapperVersion='1.3.02'
 docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=io.hkube:wrapper:$javaWrapperVersion:jar:wide -DremoteRepositories=https://oss.sonatype.org/content/repositories/snapshots -Ddest=/jars/wrapper.jar
 docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=io.hkube:interfaces:$javaWrapperVersion:jar -DremoteRepositories=https://oss.sonatype.org/content/repositories/snapshots -Ddest=/jars/interfaces.jar
 docker run --rm  -v $SCRIPTPATH/../environments/java/jars:/jars maven mvn -q dependency:get -Dartifact=io.hkube:java-algo-parent:$javaWrapperVersion:pom -DremoteRepositories=https://oss.sonatype.org/content/repositories/snapshots -Ddest=/jars/java-algo-parent.xml
