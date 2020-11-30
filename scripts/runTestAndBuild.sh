106bbd8ce87a2097f887b07f1e3b0e717561ea22
Yehiyam Livneh <yehiyam@gmail.com>
2020-11-30 14:48:28 +0200

    ci

--- scripts/runTestAndBuild.sh
+++ scripts/runTestAndBuild.sh
@@ -3,12 +3,12 @@ set -xeo pipefail
 echo running ./scripts/test.sh
 ./scripts/test.sh
 echo ./scripts/test.sh exited with code $?
-echo running ./scripts/createVersion.sh
-./scripts/createVersion.sh
-echo ./scripts/createVersion.sh exited with code $?
-echo running ./scripts/build.sh
-./scripts/build.sh
-echo ./scripts/build.sh exited with code $?
-echo running ./scripts/trigger.js
-./scripts/trigger.js
-echo ./scripts/trigger.js exited with code $?
+# echo running ./scripts/createVersion.sh
+# ./scripts/createVersion.sh
+# echo ./scripts/createVersion.sh exited with code $?
+# echo running ./scripts/build.sh
+# ./scripts/build.sh
+# echo ./scripts/build.sh exited with code $?
+# echo running ./scripts/trigger.js
+# ./scripts/trigger.js
+# echo ./scripts/trigger.js exited with code $?
