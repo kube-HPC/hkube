0580d8323f90ecbed08be299f1f030b2ce0bcd4c
Yehiyam Livneh <yehiyam@gmail.com>
2020-12-01 11:13:01 +0200

    ci - try

--- scripts/createVersion.sh
+++ scripts/createVersion.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -evo pipefail
 MESSAGE="$(git log -1 --pretty=%B) .... bump version [skip ci]"
+git remote set-url origin "https://${GH_TOKEN}@github.com/${REPO_SLUG}.git"
 git remote set-url --push origin "https://${GH_TOKEN}@github.com/${REPO_SLUG}.git"
 git remote -v
 git status
@@ -12,4 +13,5 @@ git add core/*/package-lock.json
 git add core/*/package.json 
 HUSKY_SKIP_HOOKS=1 git commit -m "${MESSAGE}" || true
 HUSKY_SKIP_HOOKS=1 npm version patch --git-tag-version --commit-hooks=false -m "${MESSAGE}"
+git remote -v
 git push origin version-branch:master --follow-tags
