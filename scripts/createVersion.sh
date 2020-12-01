bd3f96bad069226a31540253b9447e0989a74ffe
Yehiyam Livneh <yehiyam@gmail.com>
2020-12-01 09:09:59 +0200

    ci - try push

--- scripts/createVersion.sh
+++ scripts/createVersion.sh
@@ -2,6 +2,7 @@
 set -evo pipefail
 MESSAGE="$(git log -1 --pretty=%B) .... bump version [skip ci]"
 git remote set-url --push origin "https://${GH_TOKEN}@github.com/${REPO_SLUG}.git"
+git remote -v
 lerna version patch --no-push --yes --includeMergedTags --no-git-tag-version --no-commit-hooks -m "${MESSAGE}"
 lerna exec "npm install -s --ignore-scripts --package-lock-only --no-audit"
 git add core/*/package-lock.json 
