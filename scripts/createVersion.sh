75a22f66a9a242aeca5245a298ea704df05530bb
Yehiyam Livneh <yehiyam@gmail.com>
2020-12-01 10:58:39 +0200

    try

--- scripts/createVersion.sh
+++ scripts/createVersion.sh
@@ -3,10 +3,13 @@ set -evo pipefail
 MESSAGE="$(git log -1 --pretty=%B) .... bump version [skip ci]"
 git remote set-url --push origin "https://${GH_TOKEN}@github.com/${REPO_SLUG}.git"
 git remote -v
+git status
+git checkout -f -b version-branch
+git status
 lerna version patch --no-push --yes --includeMergedTags --no-git-tag-version --no-commit-hooks -m "${MESSAGE}"
 lerna exec "npm install -s --ignore-scripts --package-lock-only --no-audit"
 git add core/*/package-lock.json 
 git add core/*/package.json 
 HUSKY_SKIP_HOOKS=1 git commit -m "${MESSAGE}" || true
 HUSKY_SKIP_HOOKS=1 npm version patch --git-tag-version --commit-hooks=false -m "${MESSAGE}"
-git push --follow-tags
+git push origin version-branch:master --follow-tags
