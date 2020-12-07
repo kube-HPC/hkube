422e58075d151dc6db6341b8452d8127839ebe67
Yehiyam Livneh <yehiyam@gmail.com>
2020-12-07 14:18:23 +0200

    try fix nodejs

--- core/algorithm-builder/dockerfile/get-deps-nodejs.sh
+++ core/algorithm-builder/dockerfile/get-deps-nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -eo pipefail
+set -exo pipefail
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p $SCRIPTPATH/../environments/nodejs/packages
 cp $SCRIPTPATH/../environments/nodejs/wrapper/package.json $SCRIPTPATH/../environments/nodejs/packages/
 echo downloading for nodejs
-docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $SCRIPTPATH/../environments/nodejs/packages:/packages -v $SCRIPTPATH/../environments/nodejs/wrapper:/wrapper node:14.5.0 npm install  --prefix /packages/
+docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $SCRIPTPATH/../environments/nodejs/packages:/packages node:14.5.0 /bin/bash -c 'pwd && ls -la && npm install  --prefix /packages/'
