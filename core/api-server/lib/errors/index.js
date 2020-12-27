8d836f42abec68f143e65aa6a6c764b3d411281d
NassiHarel <nassi9@gmail.com>
2020-12-27 10:26:30 +0200

    feat: clean up

--- core/api-server/lib/errors/index.js
+++ core/api-server/lib/errors/index.js
@@ -3,13 +3,11 @@ const ResourceNotFoundError = require('./ResourceNotFoundError');
 const ResourceExistsError = require('./ResourceExistsError');
 const MethodNotAllowed = require('./MethodNotAllowed');
 const ActionNotAllowed = require('./ActionNotAllowed');
-const NotModified = require('./NotModified');
 
 module.exports = {
     InvalidDataError,
     ResourceNotFoundError,
     ResourceExistsError,
     MethodNotAllowed,
-    ActionNotAllowed,
-    NotModified
+    ActionNotAllowed
 };
