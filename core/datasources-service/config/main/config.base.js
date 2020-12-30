4fe367847d1751a52e361f06afb5d6b3278cbb12
reggev <reggev.am@gmail.com>
2020-12-30 12:00:37 +0200

    added defaults to mongo credentials

--- core/datasources-service/config/main/config.base.js
+++ core/datasources-service/config/main/config.base.js
@@ -53,8 +53,8 @@ config.db = {
     provider: 'mongo',
     mongo: {
         auth: {
-            user: process.env.MONGODB_SERVICE_USER_NAME,
-            password: process.env.MONGODB_SERVICE_PASSWORD,
+            user: process.env.MONGODB_SERVICE_USER_NAME || 'tester',
+            password: process.env.MONGODB_SERVICE_PASSWORD || 'password',
         },
         host: process.env.MONGODB_SERVICE_HOST || 'localhost',
         port: formatter.parseInt(process.env.MONGODB_SERVICE_PORT, 27017),
