542ad8fc357f781aa1c9840d76c9c4fac2be84db
golanha <golanha@rnd-hub.com>
2020-12-31 06:27:32 +0200

    fill missing data that was too large for debug algorithm

--- core/worker/lib/code-api/subpipeline/subpipeline-v1.js
+++ core/worker/lib/code-api/subpipeline/subpipeline-v1.js
@@ -1,11 +1,21 @@
 const storageManager = require('@hkube/storage-manager');
 
+async function fillMissing(element) {
+    if (element.info) {
+        if (element.info.message && element.info.message.startsWith('data too large')) {
+            const res = await storageManager.getCustomData(element.info);
+            element.result = res.payload;
+        }
+    }
+}
 class SubPipelineHandler {
     async getResultFromStorage(options) {
         let { result } = options;
         const { includeResult } = options;
         if (includeResult && result && result.storageInfo) {
             const { payload } = await storageManager.getCustomData(result.storageInfo);
+            payload.forEach(node => fillMissing(node));
+
             result = payload;
         }
         return result;
