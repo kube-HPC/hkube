7304a683735c6412ffebacf6d122480948f49bef
Nassi Harel <nassi9@gmail.com>
2020-12-31 13:55:16 +0200

    feat: enable big data debug (#1071)

--- core/worker/lib/code-api/subpipeline/subpipeline-v1.js
+++ core/worker/lib/code-api/subpipeline/subpipeline-v1.js
@@ -1,26 +1,22 @@
 const storageManager = require('@hkube/storage-manager');
 
-async function fillMissing(element) {
-    if (element.info) {
-        if (element.info.message?.startsWith('data too large')) {
-            const res = await storageManager.getCustomData(element.info);
-            element.result = res.payload;
-        }
-    }
-}
 class SubPipelineHandler {
     async getResultFromStorage(options) {
         let { result } = options;
         const { includeResult } = options;
         if (includeResult && result && result.storageInfo) {
             const { payload } = await storageManager.getCustomData(result.storageInfo);
-            for (let i = 0; i < payload.length; i += 1) {
-                // eslint-disable-next-line no-await-in-loop
-                await fillMissing(payload[i]);
-            }
+            await Promise.all(payload.map(p => this._fillMissing(p)));
             result = payload;
         }
         return result;
+    }
+
+    async _fillMissing(element) {
+        if (element?.info?.isBigData) {
+            const res = await storageManager.getCustomData(element.info);
+            element.result = res.payload;
+        }
     }
 }
 
