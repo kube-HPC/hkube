40ac9677a0388dc78af000ba1b473aee7449e5ce
golanha <golanha@rnd-hub.com>
2020-12-31 09:01:05 +0200

    fill subpipeline data for debug

--- core/worker/lib/code-api/subpipeline/subpipeline-v1.js
+++ core/worker/lib/code-api/subpipeline/subpipeline-v1.js
@@ -1,11 +1,24 @@
 const storageManager = require('@hkube/storage-manager');
 
+async function fillMissing(element) {
+    if (element.info) {
+        if (element.info.message?.startsWith('data too large')) {
+            const res = await storageManager.getCustomData(element.info);
+            // eslint-disable-next-line no-param-reassign
+            element.result = res.payload;
+        }
+    }
+}
 class SubPipelineHandler {
     async getResultFromStorage(options) {
         let { result } = options;
         const { includeResult } = options;
         if (includeResult && result && result.storageInfo) {
             const { payload } = await storageManager.getCustomData(result.storageInfo);
+            for (let i = 0; i < payload.length; i += 1) {
+                // eslint-disable-next-line no-await-in-loop
+                await fillMissing(payload[i]);
+            }
             result = payload;
         }
         return result;
