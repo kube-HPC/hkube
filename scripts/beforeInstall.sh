497c8dd6ce752beee5dc32722a1d176864168a42
reggev <reggev.am@gmail.com>
2021-01-14 13:57:42 +0200

    updated beforeInstall

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -9,3 +9,7 @@ docker run -d -p 9000:9000 --name minio1 -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMP
 docker run -d -p 27017:27017 --name mongodb -e MONGO_INITDB_ROOT_USERNAME=tester \
   -e MONGO_INITDB_ROOT_PASSWORD=password mongo:4.4.1-bionic
 docker run -d -p 3010:3010 --name gitea hkube/gitea-dev:v1.13.0-0
+
+# install dvc and git
+curl https://dvc.org/deb/dvc.list -L --output /etc/apt/sources.list.d/dvc.list
+apt update && apt install -y git-all dvc && rm -rf /var/lib/apt/lists/*
