1e4ec6a59cfb42bba6fa63ae5b4733d7cee38707
reggev <reggev.am@gmail.com>
2021-01-25 18:17:42 +0200

    lowered tests timout

--- core/datasources-service/tests/setup.js
+++ core/datasources-service/tests/setup.js
@@ -6,17 +6,12 @@ const DATASOURCE_GIT_REPOS_DIR = 'temp/git-repositories';
 
 const STORAGE_DIR = '/var/tmp/fs/storage/local-hkube-datasource';
 
-before(async function () {
-    this.timeout(15000);
+before(async () => {
     const bootstrap = require('../bootstrap');
     /** @type {import('./../lib/utils/types').config} */
     const config = await bootstrap.init();
     const storage = new storageManager.StorageManager();
-    await storage.init(
-        { ...config},
-        null,
-        true
-    );
+    await storage.init({ ...config }, null, true);
     const baseUrl = `${config.swagger.protocol}://${config.swagger.host}:${config.swagger.port}`;
     const restUrl = `${baseUrl}/${config.rest.prefix}/v1`;
     const internalUrl = `${baseUrl}/internal/v1`;
