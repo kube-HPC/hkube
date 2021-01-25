ba06a69220aa2d23e4be1cf34b2ad0d207de0e4a
yehiyam <yehiyam@users.noreply.github.com>
2021-01-25 11:48:08 +0200

    fix component (#1116)
    * fix component
    
    * fix labeler
    
    * cleanup

--- core/datasources-service/tests/setup.js
+++ core/datasources-service/tests/setup.js
@@ -1,5 +1,7 @@
 const fse = require('fs-extra');
 const sinon = require('sinon');
+const storageManager = require('@hkube/storage-manager');
+
 const DATASOURCE_GIT_REPOS_DIR = 'temp/git-repositories';
 
 const STORAGE_DIR = '/var/tmp/fs/storage/local-hkube-datasource';
@@ -9,6 +11,12 @@ before(async function () {
     const bootstrap = require('../bootstrap');
     /** @type {import('./../lib/utils/types').config} */
     const config = await bootstrap.init();
+    const storage = new storageManager.StorageManager();
+    await storage.init(
+        { ...config},
+        null,
+        true
+    );
     const baseUrl = `${config.swagger.protocol}://${config.swagger.host}:${config.swagger.port}`;
     const restUrl = `${baseUrl}/${config.rest.prefix}/v1`;
     const internalUrl = `${baseUrl}/internal/v1`;
