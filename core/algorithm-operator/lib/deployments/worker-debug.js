d353d7c59098a7a2c509fdb004c7fd1b7caaf888
yehiyam <yehiyam@users.noreply.github.com>
2021-02-10 12:29:18 +0200

    add datasource envs and mounts to debug worker (#1139)

--- core/algorithm-operator/lib/deployments/worker-debug.js
+++ core/algorithm-operator/lib/deployments/worker-debug.js
@@ -1,6 +1,6 @@
 const clonedeep = require('lodash.clonedeep');
 const log = require('@hkube/logger').GetLogFromContainer();
-const { applyEnvToContainer, applyStorage, applyImagePullSecret } = require('@hkube/kubernetes-client').utils;
+const { applyEnvToContainer, applyStorage, applyImagePullSecret, applyVolumeMounts, applyVolumes } = require('@hkube/kubernetes-client').utils;
 const { applyImage, applyJaeger } = require('../helpers/kubernetes-utils');
 const component = require('../consts/componentNames').K8S;
 const { deploymentDebugTemplate, workerIngress, workerService } = require('../templates/worker-debug');
@@ -20,6 +20,24 @@ const applyNodeSelector = (inputSpec, clusterOptions = {}) => {
     return spec;
 };
 
+const applyDataSourcesVolumes = (inputSpec) => {
+    let spec = clonedeep(inputSpec);
+    // if (!clusterOptions.dataSourcesEnabled) {
+    //     return spec;
+    // }
+    spec = applyVolumeMounts(spec, CONTAINERS.ALGORITHM_DEBUG, {
+        name: 'datasources-storage',
+        mountPath: '/hkube/datasources-storage'
+    });
+    spec = applyVolumes(spec, {
+        name: 'datasources-storage',
+        persistentVolumeClaim: {
+            claimName: 'hkube-datasources'
+        }
+    });
+    return spec;
+};
+
 const createKindsSpec = ({ algorithmName, versions, registry, clusterOptions, workerEnv, options }) => {
     if (!algorithmName) {
         const msg = 'Unable to create deployment spec. algorithmName is required';
@@ -35,6 +53,7 @@ const createKindsSpec = ({ algorithmName, versions, registry, clusterOptions, wo
     deploymentSpec = applyAlgorithmName(deploymentSpec, algorithmName, CONTAINERS.ALGORITHM_DEBUG);
     deploymentSpec = applyStorage(deploymentSpec, options.defaultStorage, CONTAINERS.ALGORITHM_DEBUG, 'algorithm-operator-configmap');
     deploymentSpec = applyJaeger(deploymentSpec, CONTAINERS.ALGORITHM_DEBUG, options);
+    deploymentSpec = applyDataSourcesVolumes(deploymentSpec);
     deploymentSpec = applyImagePullSecret(deploymentSpec, clusterOptions?.imagePullSecretName);
     const ingressSpec = workerIngress(algorithmName, clusterOptions);
     const serviceSpec = workerService(algorithmName);
