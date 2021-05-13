9e45aa080bf2bb3f0593aae5ed98d446815cb1cb
NassiHarel <nassi9@gmail.com>
2021-05-13 12:49:07 +0300

    feat: dataSource -> spec

--- core/api-server/lib/validation/pipelines.js
+++ core/api-server/lib/validation/pipelines.js
@@ -19,13 +19,6 @@ class ApiValidator {
         if (!pipeline.nodes?.length) {
             throw new InvalidDataError('pipeline must have at nodes property with at least one node');
         }
-        pipeline.nodes.forEach(node => {
-            if (node.kind === nodeKind.DataSource) {
-                if (!node.dataSource) {
-                    throw new InvalidDataError('you must provide a valid dataSource');
-                }
-            }
-        });
     }
 }
 
