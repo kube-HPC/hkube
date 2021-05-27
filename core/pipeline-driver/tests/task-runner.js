c9363f42c8584724eb06fc1fa859a5dc009992e4
NassiHarel <nassi9@gmail.com>
2021-05-27 14:40:06 +0300

    feat: driver tests

--- core/pipeline-driver/tests/task-runner.js
+++ core/pipeline-driver/tests/task-runner.js
@@ -315,7 +315,6 @@ describe('TaskRunner', function () {
         const algorithm = discovery.unScheduledAlgorithms[algorithmName];
         expect(node.status).to.equal(algorithm.reason);
         expect(node.batch[0].status).to.equal(algorithm.reason);
-        expect(node.warnings[0]).to.equal(algorithm.message);
     });
     it('should run stateful nodes at start', async function () {
         const jobId = createJobId();
