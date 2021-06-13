6f63fb3e71a5d54813cdb09ef95666bf69a4e126
golanha <golanha@rnd-hub.com>
2021-06-13 16:47:19 +0300

    test

--- core/algorithm-operator/tests/algorithm-debug.js
+++ core/algorithm-operator/tests/algorithm-debug.js
@@ -20,7 +20,7 @@ describe('jobCreator', () => {
             const { serviceSpec, ingressSpec } = createIngressServiceSpec({ algorithmName: 'myalgo1-debug', clusterOptions: { ingressHost: 'myhost' } });
             expect(ingressSpec).to.nested.include({ 'spec.rules[0].host': 'myhost' });
             expect(ingressSpec).to.nested.include({ 'spec.rules[0].http.paths[0].path': '/hkube/debug/myalgo1' });
-            expect(serviceSpec).to.nested.include({ 'spec.selector.algorithm-name': 'myalgo1' });
+            expect(serviceSpec).to.nested.include({ 'spec.selector.algorithm-name': 'myalgo1-debug' });
         });
     });
 });
