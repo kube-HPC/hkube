31100ed4cbd65cbd9e502bff2f5ae830b100aec3
golanha <golanha@rnd-hub.com>
2021-06-13 14:54:48 +0300

    service and ingress name changed

--- core/algorithm-operator/lib/reconcile/algorithm-debug.js
+++ core/algorithm-operator/lib/reconcile/algorithm-debug.js
@@ -9,8 +9,8 @@ const _createIngressService = async ({ algorithmName, clusterOptions }) => {
 
 const reconcile = async ({ services, debugAlgorithms, clusterOptions } = {}) => {
     const normServices = normalizeServices(services);
-    const added = debugAlgorithms.filter(a => !normServices.find(d => d.algorithmName === a.name));
-    const removed = normServices.filter(d => !debugAlgorithms.find(a => d.algorithmName === a.name));
+    const added = debugAlgorithms.filter(a => !normServices.find(d => `${d.algorithmName}-debug` === a.name));
+    const removed = normServices.filter(d => !debugAlgorithms.find(a => `${d.algorithmName}-debug` === a.name));
     const reconcileResult = {};
 
     for (let debug of added) { // eslint-disable-line
