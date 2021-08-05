c2936e3f3edc6d2ad0c99ecc4f40d7cc6c1f5375
yehiyam <yehiyam@users.noreply.github.com>
2021-08-05 14:10:29 +0300

    add ingress type (#1355)
    * add ingress type
    
    * update packages
    
    * update dev dependencies
    
    * Merge branch 'master' into k8s-1-22
    
    * update k8s package
    add ingressClass

--- core/algorithm-operator/tests/tensorboard.js
+++ core/algorithm-operator/tests/tensorboard.js
@@ -8,18 +8,18 @@ let createIngressServiceSpec;
 
 describe('jobCreator', () => {
     before(() => {
-        createIngressServiceSpec = require('../lib/deployments/algorithm-debug.js').createIngressServiceSpec;
+        createKindsSpec = require('../lib/deployments/tensorboard.js').createKindsSpec;
 
     })
     describe('createIngressServiceSpec', () => {
         describe('kubeVersion v1.18', () => {
             it('should include algorithm name and host', () => {
-                const { serviceSpec, ingressSpec } = createIngressServiceSpec({ algorithmName: 'myalgo1-debug', debugName: 'myalgo1', clusterOptions: { ingressHost: 'myhost' } });
+                const { serviceSpec, ingressSpec } = createKindsSpec({ boardReference: 'myalgo1-board', clusterOptions: { ingressHost: 'myhost' }, options: {} });
                 expect(ingressSpec).to.nested.include({ 'spec.rules[0].host': 'myhost' });
-                expect(ingressSpec).to.nested.include({ 'spec.rules[0].http.paths[0].path': '/hkube/debug/myalgo1' });
-                expect(ingressSpec.spec.rules[0].http.paths[0].backend).to.eql({ serviceName: 'service-debug-myalgo1-debug', servicePort: 80 });
+                expect(ingressSpec).to.nested.include({ 'spec.rules[0].http.paths[0].path': '/hkube/board/myalgo1-board' });
+                expect(ingressSpec.spec.rules[0].http.paths[0].backend).to.eql({ serviceName: 'board-service-myalgo1-board', servicePort: 80 });
                 expect(ingressSpec.spec.rules[0].http.paths[0].pathTyep).to.not.exist;
-                expect(serviceSpec).to.nested.include({ 'spec.selector.algorithm-name': 'myalgo1-debug' });
+                expect(serviceSpec).to.nested.include({ 'spec.selector.app': 'board-myalgo1-board' });
             });
         })
         describe('kubeVersion v1.22', () => {
@@ -30,19 +30,19 @@ describe('jobCreator', () => {
                 global.testParams.kubernetesMock.kubeVersion.version = 'v1.18'
             });
             it('should include algorithm name and host', () => {
-                const { serviceSpec, ingressSpec } = createIngressServiceSpec({ algorithmName: 'myalgo1-debug', debugName: 'myalgo1', clusterOptions: { ingressHost: 'myhost' } });
+                const { serviceSpec, ingressSpec } = createKindsSpec({ boardReference: 'myalgo1-board', clusterOptions: { ingressHost: 'myhost' }, options: {} });
                 expect(ingressSpec).to.nested.include({ 'spec.rules[0].host': 'myhost' });
-                expect(ingressSpec).to.nested.include({ 'spec.rules[0].http.paths[0].path': '/hkube/debug/myalgo1' });
+                expect(ingressSpec).to.nested.include({ 'spec.rules[0].http.paths[0].path': '/hkube/board/myalgo1-board' });
                 expect(ingressSpec.spec.rules[0].http.paths[0].backend).to.eql({
                     service: {
-                        name: 'service-debug-myalgo1-debug',
+                        name: 'board-service-myalgo1-board',
                         port: {
                             number: 80
                         }
                     }
                 });
                 expect(ingressSpec.spec.rules[0].http.paths[0].pathType).to.exist;
-                expect(serviceSpec).to.nested.include({ 'spec.selector.algorithm-name': 'myalgo1-debug' });
+                expect(serviceSpec).to.nested.include({ 'spec.selector.app': 'board-myalgo1-board' });
             });
         })
     });
