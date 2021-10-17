cdae24b9fdae5cd1975c24062873b5a40587bb16
golanha <golanha@rnd-hub.com>
2021-10-17 11:36:24 +0300

    add test

--- core/algorithm-output/tests/setup.js
+++ core/algorithm-output/tests/setup.js
@@ -4,9 +4,4 @@ before(async function () {
     this.timeout(15000)
     const bootstrap = require('../bootstrap');
     const config = await bootstrap.init();
-    const restUrl = `${config.swagger.protocol}://${config.swagger.host}:${config.swagger.port}`;
-    global.testParams = {
-        restUrl,
-        config
-    }
 });
