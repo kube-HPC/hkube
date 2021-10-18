2a2ac93efdf9bb48e54bb297859096df4d5ec6d3
golanha <golanha@rnd-hub.com>
2021-10-18 16:32:40 +0300

    CR

--- core/algorithm-output/config/main/config.base.js
+++ core/algorithm-output/config/main/config.base.js
@@ -4,6 +4,3 @@ const config = {};
 config.serviceName = packageJson.name;
 config.version = packageJson.version;
 module.exports = config;
-config.communication = {
-    port: process.env.WORKER_CLIENT_SOCKET_PORT || 3005
-};
