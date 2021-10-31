75e3eccd40c157e561cd75b4a2053f38a63cc6f2
NassiHarel <nassi9@gmail.com>
2021-10-31 10:45:48 +0200

    feat: improve validation

--- core/api-server/lib/service/algorithms.js
+++ core/api-server/lib/service/algorithms.js
@@ -109,14 +109,22 @@ class AlgorithmStore {
     }
 
     _entitiesToText(entities) {
-        return Object.entries(entities).filter(([, v]) => v).map(([k, v]) => `${v} ${k}`).join(', ');
+        return Object.entries(entities)
+            .filter(([, v]) => v)
+            .map(([k, v]) => {
+                if (Array.isArray(v)) {
+                    return `${v.length} ${k} (${v.join(',')})`;
+                }
+                return `${v} ${k}`;
+            })
+            .join(', ');
     }
 
     async _checkAlgorithmDependencies({ name, pipelines, executions }) {
         let message;
         const entities = {
-            pipelines: pipelines.length,
-            executions: executions.length
+            pipelines: pipelines.map(p => p.name),
+            executions: executions.map(e => e.jobId),
         };
         const details = this._entitiesToText(entities);
 
