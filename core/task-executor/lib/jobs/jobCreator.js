363b6ba07cbb407e2f699e808bda90204121d604
golanha <golanha@rnd-hub.com>
2021-11-08 13:27:52 +0200

    set env variable

--- core/task-executor/lib/jobs/jobCreator.js
+++ core/task-executor/lib/jobs/jobCreator.js
@@ -11,7 +11,7 @@ const parse = require('@hkube/units-converter');
 const { components, containers, gpuVendors, volumes: volumeKinds } = require('../consts');
 const { JAVA } = require('../consts/envs');
 const component = components.K8S;
-const { workerTemplate, gatewayEnv, logVolumes, logVolumeMounts, sharedVolumeMounts, algoMetricVolume } = require('../templates');
+const { optimizerEnv, workerTemplate, gatewayEnv, logVolumes, logVolumeMounts, sharedVolumeMounts, algoMetricVolume } = require('../templates');
 const { settings } = require('../helpers/settings');
 const CONTAINERS = containers;
 
@@ -388,6 +388,7 @@ const createJobSpec = ({ kind, algorithmName, resourceRequests, workerImage, alg
         spec = applyEnvToContainer(spec, CONTAINERS.ALGORITHM, gatewayEnv);
     }
     if (kind === nodeKind.Optimizer) {
+        spec = applyEnvToContainer(spec, CONTAINERS.ALGORITHM, optimizerEnv);
         spec = applyDatascienceMetricsVolumes(spec);
     }
     spec = applyLabels(spec, labels);
