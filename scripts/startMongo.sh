b305e179b5e4f4498553540dc60df4d150b70b3e
Yehiyam Livneh <yehiyam@gmail.com>
2022-01-19 16:07:13 +0200

    tests

--- scripts/startMongo.sh
+++ scripts/startMongo.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+# KEY_FILE=/tmp/mongo.keyfile
+KEY_FILE=$(mktemp)
+openssl rand -base64 756 > $KEY_FILE
+sudo chown 999 $KEY_FILE
+sudo chmod 400 $KEY_FILE
 docker rm -f mongodb
 docker run -d --name mongodb \
       -p 27017:27017 \
       -e MONGO_INITDB_ROOT_USERNAME=tester \
-      -e MONGO_INITDB_ROOT_PASSWORD=password mongo:4.4.1-bionic --replSet rs0
+      -e MONGO_INITDB_ROOT_PASSWORD=password \
+      -e KEY_FILE=$KEY_FILE \
+      -v $KEY_FILE:$KEY_FILE \
+      mongo:4.4.1-bionic --replSet rs0 --keyFile $KEY_FILE
 
 INIT=$(docker exec mongodb sh -c 'echo "rs.initiate().ok || rs.status().ok"|mongo -u tester -p password --quiet' | tr -d '\r')
 COUNT=1
