6782eed31f0ee7426d2d640fcd236755cba70436
yehiyam <yehiyam@users.noreply.github.com>
2022-02-21 10:18:19 +0200

    handle pipeline-driver scale (#1530)

--- core/resource-manager/lib/adapters/drivers/queue.js
+++ core/resource-manager/lib/adapters/drivers/queue.js
@@ -1,6 +1,6 @@
 const Adapter = require('../Adapter');
 const stateManager = require('../../store/store-manager');
-
+const combinedQueue = 'pipeline-driver';
 class PipelineDriversQueueAdapter extends Adapter {
     constructor(options) {
         super(options);
@@ -9,9 +9,13 @@ class PipelineDriversQueueAdapter extends Adapter {
     async _getData() {
         const queue = await stateManager.getPipelineDriverQueue();
         queue.forEach(al => {
-            al.data = al.data.map(a => ({ name: al.name, score: a.score }));
+            al.data = al.data.map(a => ({ name: al.name, score: a }));
         });
-        return queue;
+        const combined = [{ name: combinedQueue, data: [] }];
+        queue.forEach(al => {
+            combined[0].data = combined[0].data.concat(al.data.map(a => ({ ...a, name: combinedQueue })));
+        });
+        return combined;
     }
 }
 
