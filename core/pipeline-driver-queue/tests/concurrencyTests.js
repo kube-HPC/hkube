3010f64215f5297ed5e585e97f480cbce074c38b
Yehiyam Livneh <yehiyam@gmail.com>
2022-02-27 17:34:01 +0200

    concurrency tests

--- core/pipeline-driver-queue/tests/concurrencyTests.js
+++ core/pipeline-driver-queue/tests/concurrencyTests.js
@@ -1,96 +1,122 @@
 const { expect } = require('chai');
-const sinon = require('sinon');
 const delay = require('await-delay');
-const { generateArr, stubTemplate } = require('./stub/stub');
 const { uid: uuidv4 } = require('@hkube/uid');
-const { Producer } = require('@hkube/producer-consumer');
-const queueEvents = require('../lib/consts/queue-events');
-const { semaphore } = require('await-done');
 const { pipelines } = require('./mock/index');
 const bootstrap = require('../bootstrap');
 const queueRunner = require('../lib/queue-runner');
+const producerLib = require('../lib/jobs/producer')
 const dataStore = require('../lib/persistency/data-store');
-const producerLib = require('../lib/jobs/producer');
-const setting = { prefix: 'pipeline-driver-queue' }
-const producer = new Producer({ setting });
-const Queue = require('../lib/queue');
-const heuristic = score => job => ({ ...job, entranceTime: Date.now(), score, ...{ calculated: { latestScore: {} } } })
+
 const heuristicStub = score => job => ({ ...job })
-const heuristicBoilerPlate = (score, _heuristic) => ({
-    run(job) {
-        return _heuristic(score)(job);
-    }
-});
-
-let queue = null;
-let consumer;
-let _semaphore = null;
-
+let getActiveFromRedisOriginal = null;
 describe('Concurrency', () => {
-    before(async () => {
-        consumer = global.consumer;
-    });
-    beforeEach(() => {
-        queue = new Queue();
-        queue.updateHeuristic({ run: heuristic(80) });
-        producerLib._isConsumerActive = false;
-        _semaphore = new semaphore();
-    });
-    afterEach(() => {
+    beforeEach(async () => {
+        queueRunner.queue.queue = [];
         producerLib._isConsumerActive = true;
+        getActiveFromRedisOriginal = producerLib._concurrencyHandler.getJobCountsFromRedis;
+        queueRunner.queue.updateHeuristic({ run: heuristicStub() });
     });
+    afterEach(async () => {
+        queueRunner.queue.updateHeuristic(queueRunner.heuristicRunner);
+        producerLib._concurrencyHandler.getJobCountsFromRedis = getActiveFromRedisOriginal;
 
-    describe('concurrent', () => {
-        it('check concurrency limit', async () => {
-            queueRunner.queue.queue = [];
-            const totalJobs = 10;
-            const half = totalJobs / 2;
-            const keys = Array.from(Array(totalJobs).keys());
-            const jobsList = [];
-            producerLib._isConsumerActive = true;
-            let spy = sinon.spy(producerLib, 'createJob');
+    });
+    describe('concurrency', () => {
+        it('should found and disable concurrent exceeded jobs', async () => {
+            const jobs = 10;
+            const pipelineName = 'pipeline-concurrent';
+            const experimentName = 'experiment-concurrent'
+            const pipeline = pipelines.find(p => p.name === pipelineName);
+            await dataStore._db.pipelines.patch(pipeline);
 
-            // creating 10 jobs which half are maxExceeded
-            await Promise.all(keys.map(async (i) => {
-                const jobId = uuidv4();
+            let redisJobCount=1
+            producerLib._concurrencyHandler.getJobCountsFromRedis = async () => ({
+                [pipelineName]: redisJobCount
+            })
+            for (let i = 0; i < jobs; i++) {
                 const job = {
-                    data: { jobId },
-                    done: () => { }
+                    jobId: uuidv4(),
+                    maxExceeded: i > 0,
+                    done: () => { },
+                    pipelineName,
+                    experimentName,
+                    priority: 3,
+                    entranceTime: Date.now(),
+                    calculated: {
+                        latestScores: {}
+                    }
                 };
-                const maxExceeded = i % 2 === 0;
-                const pipeline = {
-                    jobId,
-                    experimentName: 'test',
-                    name: 'test',
-                    maxExceeded
-                };
-                const status = {
-                    status: 'pending'
-                };
-                jobsList.push({ jobId, maxExceeded, experiment: 'test', pipeline: 'test' });
-                await dataStore._db.jobs.create({ jobId, pipeline, status });
-                await consumer._handleJob(job);
-            }));
-
-            const nonExceededCount = spy.callCount;
-            sinon.restore();
-            spy = sinon.spy(producerLib, 'createJob');
-            const jobs = jobsList.slice(0, half);
-
-            // simulate that half of jobs are completed
-            let index = 0;
-            while (jobs.length) {
-                const job = jobs.pop();
-                producerLib._isConsumerActive = true;
-                producerLib._producer.emit('job-completed', { options: { data: job } });
-                expect(spy.callCount).to.eql(++index);
-                await delay(1);
+                queueRunner.queue.enqueue(job);
             }
-            await delay(500);
-            const exceededCount = spy.callCount;
-            expect(nonExceededCount).to.eql(half);
-            expect(exceededCount).to.eql(half);
+            await delay(300);
+            let result = await producerLib._concurrencyHandler._checkConcurrencyJobs(queueRunner.queue);
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(1,'1111')
+            expect(result).to.eql(1);
+            await delay(300);
+            producerLib._isConsumerActive = true;
+            result = await producerLib._concurrencyHandler._checkConcurrencyJobs(queueRunner.queue);
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(2, '2222')
+            await delay(300);
+            producerLib._isConsumerActive = true;
+            redisJobCount=2
+            result = await producerLib._concurrencyHandler._checkConcurrencyJobs(queueRunner.queue);
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(3, '3333')
+            await producerLib._dequeueJobInternal();
+            await producerLib._dequeueJobInternal();
+            await producerLib._dequeueJobInternal();
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(3, '4444')
+            // checking jobs will get updated values from etcd
+            producerLib._isConsumerActive = false;
+            result = await producerLib._concurrencyHandler._checkConcurrencyJobs(queueRunner.queue);
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(2, '5555')
+        });
+
+        it('should not dequeue job if active is higher than concurrency', async () => {
+            const jobs = 10;
+            const pipelineName = 'pipeline-concurrent';
+            const experimentName = 'experiment-concurrent'
+            const pipeline = pipelines.find(p => p.name === pipelineName);
+            await dataStore._db.pipelines.patch(pipeline);
+            let redisJobCount=6
+            producerLib._concurrencyHandler.getJobCountsFromRedis = async () => ({
+                [pipelineName]: redisJobCount
+            })
+
+            for (let i = 0; i < jobs; i++) {
+                const job = {
+                    jobId: uuidv4(),
+                    maxExceeded: i > 0,
+                    done: () => { },
+                    pipelineName,
+                    experimentName,
+                    priority: 3,
+                    entranceTime: Date.now(),
+                    calculated: {
+                        latestScores: {}
+                    }
+                };
+                queueRunner.queue.enqueue(job);
+            }
+            expect(queueRunner.queue.size).to.eql(jobs-1);
+
+            await delay(300);
+            producerLib._isConsumerActive = true;
+            let result = await producerLib._concurrencyHandler._checkConcurrencyJobs(queueRunner.queue);
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(6)
+            expect(result).to.eql(0);
+            expect(queueRunner.queue.size).to.eql(jobs-1);
+
+            redisJobCount=1
+            await delay(300);
+            result = await producerLib._concurrencyHandler._checkConcurrencyJobs(queueRunner.queue);
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(2)
+            expect(result).to.eql(1);
+            expect(queueRunner.queue.size).to.eql(jobs - 2);
+            await delay(300);
+            producerLib._isConsumerActive = true;
+            await producerLib._concurrencyHandler._checkMaxExceeded({ experimentName, pipelineName }, false, queueRunner.queue);
+            expect(queueRunner.queue.size).to.eql(jobs - 3);
+            expect(producerLib._concurrencyHandler._activeState[pipelineName].count).to.eql(2)
         });
     });
-
 });
