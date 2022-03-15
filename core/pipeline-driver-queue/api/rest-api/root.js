90d1ba78df45578df3289ba95fbff1b623368a13
golanha <golanha@rnd-hub.com>
2022-03-15 11:29:12 +0200

    CR

--- core/pipeline-driver-queue/api/rest-api/root.js
+++ core/pipeline-driver-queue/api/rest-api/root.js
@@ -4,12 +4,6 @@ const preferredQueue = require('../../lib/service/preferred-jobs');
 
 const routes = () => {
     const router = RestServer.router();
-    router.get('/', (req, res) => {
-        const { pageSize: pageSizeStr, firstJobId, lastJobId, tag, pipelineName, lastJobs } = req.query;
-        const pageSize = parseInt(pageSizeStr, 10);
-        const response = managedQueue.getFlatJobsList(pageSize, firstJobId, lastJobId, pipelineName, tag, lastJobs);
-        res.json(response);
-    });
     router.get('/count/', (req, res) => {
         const managed = managedQueue.getCount();
         const preferred = preferredQueue.getCount();
