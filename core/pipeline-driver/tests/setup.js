465545037896e0b15e24ac9f8aab665daa976641
golanha <golanha@rnd-hub.com>
2022-04-06 17:34:02 +0300

    add tests and upgrade db version

--- core/pipeline-driver/tests/setup.js
+++ core/pipeline-driver/tests/setup.js
@@ -5,8 +5,12 @@ const { Factory } = require('@hkube/redis-utils');
 const bootstrap = require('../bootstrap');
 const stateManager = require('../lib/state/state-manager');
 const { main: config } = configIt.load();
+const dbConnect = require('@hkube/db');
 
 before(async () => {
+    const { provider, ...options } = config.db;
+    this._db = dbConnect(options, provider);
+    await this._db.init({ createIndices: true });
     await storageManager.init(config, null, true);
     await bootstrap.init();
     const redis = Factory.getClient(config.redis);
