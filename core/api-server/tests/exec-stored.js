f82300755f573d9897e9a95a584014c0a845369d
yehiyam <yehiyam@users.noreply.github.com>
2022-04-12 14:30:50 +0300

    use debugOverride to run caching with debug (#1572)

--- core/api-server/tests/exec-stored.js
+++ core/api-server/tests/exec-stored.js
@@ -191,6 +191,12 @@ describe('Executions', () => {
             expect(res2.body.nodes[3].kind).to.eql('debug');
         });
         it('should update debug algorithm modified time on execute', async () => {
+            const optionsGET = {
+                uri: `${restUrl}/store/algorithms/yellow-alg-debug`,
+                method: 'GET'
+            };
+            // delete debug algorithms
+            await request({method: 'DELETE', uri: `${restUrl}/store/algorithms/yellow-alg-debug?force=true`});
             const options = {
                 uri: restPath,
                 body: {
@@ -204,11 +210,8 @@ describe('Executions', () => {
                 }
             };
             await request(options);
-            const optionsGET = {
-                uri: `${restUrl}/store/algorithms/yellow-alg-debug`,
-                method: 'GET'
-            };
-            let res2 = await request(optionsGET);
+
+            res2 = await request(optionsGET);
             expect(Math.abs(res2.body.created - res2.body.modified)).to.closeTo(0, 100);
             await delay(1000)
             await request(options);
