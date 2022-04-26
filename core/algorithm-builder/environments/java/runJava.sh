54d27fdc16abbbee2a11688c5e5672e811b1ba1f
Yehiyam Livneh <yehiyam@gmail.com>
2022-04-26 18:07:32 +0300

    fix log file path

--- core/algorithm-builder/environments/java/runJava.sh
+++ core/algorithm-builder/environments/java/runJava.sh
@@ -10,4 +10,4 @@ else
 fi
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/x86_64-linux-gnu/jni/
 
-mkfifo /tmp/pipe; (tee -a /var/log/etcd.log < /tmp/pipe & ) ; exec java -Xms${MIN_MEM} -Xmx${MAX_MEM} -Dcom.amazonaws.sdk.disableCertChecking=true -jar wrapper.jar algorithm_unique_folder/encapsulated-algorithm.jar > /tmp/pipe 2>&1
+mkfifo /tmp/pipe; (tee -a /hkube-logs/stdout.log < /tmp/pipe & ) ; exec java -Xms${MIN_MEM} -Xmx${MAX_MEM} -Dcom.amazonaws.sdk.disableCertChecking=true -jar wrapper.jar algorithm_unique_folder/encapsulated-algorithm.jar > /tmp/pipe 2>&1
