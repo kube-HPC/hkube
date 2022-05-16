c244aeddc01fd9500c90d2758953820619616481
Golan Hallel <golanha@rnd-hub.com>
2022-05-16 09:53:41 +0300

    add information from task (#1587)

--- core/pipeline-driver/lib/tasks/cache-pipeline.js
+++ core/pipeline-driver/lib/tasks/cache-pipeline.js
@@ -2,32 +2,35 @@ const { parser } = require('@hkube/parsers');
 const stateManager = require('../state/state-manager');
 
 class Cache {
+    constructor() {
+        this.cacheJobId = undefined;
+    }
+
     async checkCachePipeline(nodes) {
         const node = nodes.find(n => n.cacheJobId);
         if (node) {
+            this.cacheJobId = node.cacheJobId;
             const jobId = node.cacheJobId;
             const graph = await stateManager.getGraph({ jobId });
             if (!graph) {
                 throw new Error(`unable to find graph for job ${jobId}`);
             }
-            nodes.forEach((n) => {
-                this._getResultFromPredecessors(graph, n);
-            });
+            await Promise.all(nodes.map((n) => this._getResultFromPredecessors(graph, n, node.cacheJobId)));
             return true;
         }
         return false;
     }
 
-    _getResultFromPredecessors(graph, node) {
+    async _getResultFromPredecessors(graph, node, cacheJobId) {
         const nodes = this._splitInputToNodes(node.input);
         const parentOutput = [];
-        nodes.forEach((n) => {
-            const data = this._getNodeResult(graph, n.nodeName);
+        await Promise.all(Array.from(nodes, (async ([, n]) => {
+            const data = await this._getNodeResult(graph, n.nodeName, cacheJobId);
             if (data) {
                 data.type = n.type;
                 parentOutput.push(data);
             }
-        });
+        })));
         node.parentOutput = parentOutput.length === 0 ? null : parentOutput;
     }
 
@@ -42,15 +45,15 @@ class Cache {
         return newNodes;
     }
 
-    _getNodeResult(graph, nodeName) {
+    async _getNodeResult(graph, nodeName, cacheJobId) {
         let result;
         const node = graph.nodes.find(n => n.nodeName === nodeName);
-
-        if (node.batch && node.batch.length > 0) {
-            result = { node: nodeName, result: node.batch.map(b => b.output) };
+        const tasks = await stateManager.getTasks({ jobId: cacheJobId, nodeName });
+        if (node.batchInfo) {
+            result = { node: nodeName, result: tasks.map(b => b.output) };
         }
         else {
-            result = { node: nodeName, result: node.output };
+            result = { node: nodeName, result: tasks[0].result };
         }
         return result;
     }
