6f9a5a9a733151fac7d25aff5fb536558f5c76b9
ziv glik <zivglik@gmail.com>
2023-02-05 13:36:18 +0200

    fix tests

--- core/api-server/tests/graphql/queries/pipeline-query.js
+++ core/api-server/tests/graphql/queries/pipeline-query.js
@@ -3,45 +3,7 @@ const { gql } = require('graphql-request');
 
 const query = gql`{ 
     pipelines {
-                    list {
-                    kind
-                    name
-                    priority
-                    experimentName
-                    triggers {
-                        cron {
-                        enabled
-                        pattern
-                        }
-                    }
-                    options {
-                        debug
-                        pending
-                        batchTolerance
-                        progressVerbosityLevel
-                        ttl
-                        concurrentPipelines {
-                        amount
-                        rejectOnFailure
-                        }
-                    }
-                    nodes {
-                        nodeName
-                        algorithmName
-                        ttl
-                        includeInResult
-                        batchOperation
-                        metrics {
-                        tensorboard
-                        }
-                        retry {
-                        policy
-                        limit
-                        }
-                    }
-                
-                    flowInput
-                }
+                    list 
                     pipelinesCount
                    }
                 }
