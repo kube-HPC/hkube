e30b2638017c3480d723b5386c347d5196a418c0
ronshv <lepilmen@gmail.com>
2023-06-11 15:03:43 +0300

    add queue time to registerable zero values in metrics

--- core/pipeline-driver/lib/tasks/task-runner.js
+++ core/pipeline-driver/lib/tasks/task-runner.js
@@ -693,7 +693,7 @@ class TaskRunner {
         Object.entries(streamingMetricToPropMap).forEach(([key, val]) => {
             // Key represents the metric name suffix for prometheus, value - the prop to fetch
             // eslint-disable-next-line max-len
-            if ((metric[val] !== 0) || key === 'queue_size') { // Remove zero metrics to avoid wrong treadlines, except for queue_size.
+            if ((metric[val] !== 0) || key === 'queue_size' || key === 'queue_time') { // Remove zero metrics to avoid wrong treadlines, with exceptions
                 pipelineMetrics.setStreamingEdgeGaugeMetric(
                     { value: metric[val],
                         pipelineName: this._pipeline.name,
