cd2503bf50506dacade15980087b10bbd3a22495
ronshv <lepilmen@gmail.com>
2023-06-11 17:37:28 +0300

    Added new metric, and new flow for general metrics for streaming. Added object properties to simplify adding conditionals.

--- core/pipeline-driver/lib/consts/metricsNames.js
+++ core/pipeline-driver/lib/consts/metricsNames.js
@@ -7,12 +7,33 @@ module.exports = {
         streaming_edge_queueSize: 'pipeline_driver_streaming_edge_queue_size',
         streaming_edge_throughput: 'pipeline_driver_streaming_edge_throughput',
         streaming_edge_queueTimeMs: 'pipeline_driver_streaming_edge_queue_time',
-        streaming_edge_processingTimeMs: 'pipeline_driver_streaming_edge_processing_time'
+        streaming_edge_processingTimeMs: 'pipeline_driver_streaming_edge_processing_time',
+
+        streaming_pods_per_node: 'pipeline_driver_streaming_pods_per_node',
     },
-    streamingMetricToPropMap: {
-        queue_size: 'queueSize',
-        throughput: 'throughput',
-        queue_time: 'queueTimeMs',
-        processing_time: 'processingTimeMs'
+    streamingEdgeMetricToPropMap: {
+        queue_size: {
+            propName: 'queueSize',
+            registerZeroValue: true
+        },
+        throughput: {
+            propName: 'throughput',
+            registerZeroValue: false
+        },
+        queue_time: {
+            propName: 'queueTimeMs',
+            registerZeroValue: true
+        },
+        processing_time: {
+            propName: 'processingTimeMs',
+            registerZeroValue: false
+        },
+    },
+    streamingGeneralMetricToPropMap: {
+        pods_per_node: {
+            propName: 'queueSize',
+            registerZeroValue: true
+        }
     }
+
 };
