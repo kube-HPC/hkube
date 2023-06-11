0d2895788e8c67f48ecc01e47a67ccdcea7dd9dd
ronshv <lepilmen@gmail.com>
2023-06-11 14:38:23 +0300

    ignore 0 values for most streaming metrics.

--- core/pipeline-driver/lib/tasks/task-runner.js
+++ core/pipeline-driver/lib/tasks/task-runner.js
@@ -693,15 +693,17 @@ class TaskRunner {
         Object.entries(streamingMetricToPropMap).forEach(([key, val]) => {
             // Key represents the metric name suffix for prometheus, value - the prop to fetch
             // eslint-disable-next-line max-len
-            pipelineMetrics.setStreamingEdgeGaugeMetric(
-                { value: metric[val],
-                    pipelineName: this._pipeline.name,
-                    pipelineId: this._pipeline.jobId,
-                    source: metric.source,
-                    target: metric.target,
-                    status: metric.status },
-                key
-            );
+            if ((metric[val] !== 0) || key === 'queue_size') { // Remove zero metrics to avoid wrong treadlines, except for queue_size.
+                pipelineMetrics.setStreamingEdgeGaugeMetric(
+                    { value: metric[val],
+                        pipelineName: this._pipeline.name,
+                        pipelineId: this._pipeline.jobId,
+                        source: metric.source,
+                        target: metric.target,
+                        status: metric.status },
+                    key
+                );
+            }
         });
     }
 
