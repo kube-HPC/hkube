5782d3303eca2cf1fe8d24003142381243ddfd27
ido reshef <idoreshef10@gmail.com>
2023-09-20 09:42:11 +0300

    write test until expect to see what to expect

--- core/datasources-service/tests/saveMidJob.test.js
+++ core/datasources-service/tests/saveMidJob.test.js
@@ -28,16 +28,25 @@ describe('Save Mid Pipeline', () => {
         jobConsumer = require('../lib/service/jobs-consumer');
         storageManager = require('@hkube/storage-manager');
         rootDir = getDatasourcesInUseFolder(global.testParams.config);
+        restUrl = global.testParams.restUrl;
     
     });
     after(() => {
         fse.remove(rootDir);
     });
 
-    it('should add new file to the dvc track', async () => {
+    it.skip('should add new file to the dvc track', async () => {
         const name = uuid();
         const { body: dataSource } = await createDataSource(name);
         const job = await createJob({ dataSource });
-        const { jobId } = job.data;
+        const { jobId, nodeName } = job.data;
+        const dsPath = pathLib.join(rootDir, jobId, name, 'complete');
+        const newFilePath = pathLib.join(dsPath, 'data', 'a.txt');
+        await fse.outputFile(newFilePath, 'testing');
+        const options = {
+            uri: `${restUrl}/${jobId}/${name}/${nodeName}`,
+            method: 'POST'}
+        const response = await request(options);
+
     })
 })
