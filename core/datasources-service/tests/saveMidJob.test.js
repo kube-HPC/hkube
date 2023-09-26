311138fcf12c0524873e58da1e927cdd80a2a8ec
ido reshef <idoreshef10@gmail.com>
2023-09-26 13:25:36 +0300

    create unit tests for both cases

--- core/datasources-service/tests/saveMidJob.test.js
+++ core/datasources-service/tests/saveMidJob.test.js
@@ -127,4 +127,46 @@ describe('Save Mid Pipeline', () => {
 
         })
 
+        it.skip('modify file and should save', async () => {
+            const name = uuid()
+        const {body: dataSource} = await createDataSource(name, {
+            fileNames: ['logo.svg', 'logo.svg.meta'],
+        })
+        const job = await createJob({ dataSource });
+        const { jobId, nodeName } = job.data;
+        const dsPath = pathLib.join(rootDir, jobId, name, 'complete');
+        const newFilePath = pathLib.join(dsPath, 'data', 'a.txt');
+        let triesCount = 0;
+        while( true ){
+            if (await fse.pathExists(pathLib.join(dsPath, 'data'))){
+                break;
+            }
+            else{
+                await sleep(1000);
+                triesCount+=1;
+            }
+            if (triesCount > 10){
+                throw Error(`path ${dsPath} is not being created by job`)
+            }
+        }
+        await fse.outputFile(newFilePath, 'testing');
+        const options = {
+            uri: `${restUrl}/datasource/${jobId}/${name}/${nodeName}`,
+            method: 'POST'}
+        const response = await request(options);
+        expect(response.response.statusCode).to.eq(200);
+        expect(response.body).to.be.an('object');
+        expect(response.body.name).to.eq(name);
+        expect(response.body.files).to.be.an('array');
+        const nameList = response.body.files.map(file => file.name);
+        expect(nameList).to.include('a.txt');
+        const aFile = response.body.files.filter(file => file.name === 'a.txt');
+        expect(aFile[0].meta).to.eq('');
+
+        })
+
+        it.skip('delete file and should save', async () => {
+
+        })
+
 })
