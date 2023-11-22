6a965a8c87f68c35a30e8701a5b5c4bbeabfced5
ronshv <lepilmen@gmail.com>
2023-11-22 13:49:43 +0200

    prevent initial scareup being lower then minStatelessCount

--- core/worker/lib/streaming/services/auto-scaler.js
+++ core/worker/lib/streaming/services/auto-scaler.js
@@ -48,7 +48,7 @@ class AutoScaler {
         this._config = options.config;
         this._isStateful = options.node.stateType === stateType.Stateful;
         this._statelessCountLimits = { minStateless: options.node.minStatelessCount, maxStateless: options.node.maxStatelessCount };
-        this._limitActionType = { minStateless: 'minStatelessCount', maxStateless: 'maxStatelessCount' };
+        this._limitActionType = { minStateless: 'minStatelessCount', maxStateless: 'maxStatelessCount', both: 'bothLimits' };
         this._interventionLogCallTrack = { action: null, required: null, allowed: null, timeStamp: null };
         this._onSourceRemove = onSourceRemove;
         this.reset();
@@ -216,7 +216,7 @@ class AutoScaler {
         log.info(`scaling ${action} ${replicas} replicas for node ${this._nodeName} from ${currentSize} to ${scaleTo} replicas`, { component });
     }
 
-    _scalingInterventionLog(action, required, allowed) {
+    _scalingInterventionLog(action, required, allowed, customMessage = '') {
         const currentTime = Date.now();
         // Log-spam prevention
         if (this._interventionLogCallTrack.timeStamp) {
@@ -229,7 +229,11 @@ class AutoScaler {
             }
         }
         this._interventionLogCallTrack = { timeStamp: currentTime, action, required, allowed };
-        log.info(`scaling ${action} intervention, node ${this._nodeName} changed from required ${required} to ${allowed.type} ${allowed.size} `, { component });
+        if (action === this._limitActionType.both) {
+            log.info(`scaling ${action} intervention, node ${this._nodeName} changed from required ${required}. ${customMessage}`, { component });
+            return;
+        } // custom message for first intervention that handles required in both limits.
+        log.info(`scaling ${action} intervention, node ${this._nodeName} changed from required ${required} to ${allowed.type}:${allowed.size}. ${customMessage}`, { component });
     }
 
     _getScaleDetails({ reqRate, resRate, totalRequests, totalResponses, durationsRate, queueSize, avgQueueSize, currentSize }) {
@@ -245,25 +249,25 @@ class AutoScaler {
         // first scale up
         if (totalRequests > 0 && totalResponses === 0 && currentSize === 0) {
             required = this._config.scaleUp.replicasOnFirstScale;
-            required = this._capScaleByLimits(required, this._limitActionType.maxStateless);
+            required = this._capScaleByLimits(required, this._limitActionType.both, 'Based on total requests, with initial size 0');
         }
         // scale up based on durations
         else if (totalRequests > 0 && currentSize < requiredByDuration) {
             required = requiredByDuration;
-            required = this._capScaleByLimits(required, this._limitActionType.maxStateless);
+            required = this._capScaleByLimits(required, this._limitActionType.both, 'Based on total requests by duration');
         }
 
         // scale down based on stop streaming
         else if (idleScaleDown.scale && currentSize > 0 && canScaleDown) {
             required = 0;
-            required = this._capScaleByLimits(required, this._limitActionType.minStateless);
+            required = this._capScaleByLimits(required, this._limitActionType.minStateless, 'Based on idle scaledown, canScaleDown');
         }
         // scale down based on rate
         else if (!idleScaleDown.scale && currentSize > requiredByDuration && canScaleDown) {
             const diff = relDiff(requiredByDuration, this._scaler.required);
             if (diff > this._config.scaleDown.tolerance && requiredByDuration > 0) {
                 required = requiredByDuration;
-                required = this._capScaleByLimits(required, this._limitActionType.minStateless);
+                required = this._capScaleByLimits(required, this._limitActionType.minStateless, 'Based on rate');
             }
         }
         if (required !== null) {
@@ -272,20 +276,27 @@ class AutoScaler {
         return result;
     }
 
-    _capScaleByLimits(required, type) {
+    _capScaleByLimits(required, type, customMessage = '') {
+        // eslint-disable-next-line no-unused-vars
+        let decision = required;
         if (type === this._limitActionType.maxStateless && this._statelessCountLimits.maxStateless) {
             if (required > this._statelessCountLimits.maxStateless) {
-                this._scalingInterventionLog('up', required, { type: this._limitActionType.maxStateless, size: this._statelessCountLimits.maxStateless });
+                this._scalingInterventionLog('up', required, { type: this._limitActionType.maxStateless, size: this._statelessCountLimits.maxStateless }, customMessage);
                 return this._statelessCountLimits.maxStateless;
             }
         }
         else if (type === this._limitActionType.minStateless && this._statelessCountLimits.minStateless) {
             if (required < this._statelessCountLimits.minStateless) {
-                this._scalingInterventionLog('down', required, { type: this._limitActionType.minStateless, size: this._statelessCountLimits.minStateless });
+                this._scalingInterventionLog('down', required, { type: this._limitActionType.minStateless, size: this._statelessCountLimits.minStateless }, customMessage);
                 return this._statelessCountLimits.minStateless;
             }
         }
-        return required;
+        else if (type === this._limitActionType.both) {
+            if (this._statelessCountLimits.minStateless) decision = Math.max(decision, this._statelessCountLimits.minStateless);
+            if (this._statelessCountLimits.maxStateless) decision = Math.min(decision, this._statelessCountLimits.maxStateless);
+            this._scalingInterventionLog(this._limitActionType.both, required, { type: this._limitActionType.both, size: undefined }, customMessage);
+        } // Govern both limits
+        return decision;
     }
 
     _addExtraReplicas(requiredByDurationRate, requiredByQueueSize) {
