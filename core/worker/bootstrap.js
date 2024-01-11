5ec1f33de5cb180e3a5a670b51b2f1b2adf68c77
ido reshef <idoreshef10@gmail.com>
2024-01-11 15:48:10 +0200

    change to log.info

--- core/worker/bootstrap.js
+++ core/worker/bootstrap.js
@@ -41,7 +41,7 @@ class Bootstrap {
             monitor.on('close', (data) => {
                 log.error(data.error.message, { component });
                 jobConsumer.sendWarning(data.error.message);
-                log.message('The status has been changed to warning')
+                log.info('The status has been changed to warning')
                 worker.handleExit(1);
             });
             await monitor.check(main.redis);
