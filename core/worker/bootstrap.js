9d45bee65d9ec29f290236c66011cc40b4c0cba9
ido reshef <idoreshef10@gmail.com>
2024-01-11 08:52:18 +0200

    adding log before handle exit to show that the status has been changed

--- core/worker/bootstrap.js
+++ core/worker/bootstrap.js
@@ -41,6 +41,7 @@ class Bootstrap {
             monitor.on('close', (data) => {
                 log.error(data.error.message, { component });
                 jobConsumer.sendWarning(data.error.message);
+                log.message('The status has been changed to warning')
                 worker.handleExit(1);
             });
             await monitor.check(main.redis);
