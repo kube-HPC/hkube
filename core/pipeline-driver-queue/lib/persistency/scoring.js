6de233322548c7d677fbebf2bb4b0d7eac3114f5
ronshv <lepilmen@gmail.com>
2024-04-16 10:57:38 +0300

    Added maxExceeded array to tag maxExceeded concurrent jobs

--- core/pipeline-driver-queue/lib/persistency/scoring.js
+++ core/pipeline-driver-queue/lib/persistency/scoring.js
@@ -6,8 +6,8 @@ class Scoring {
         try {
             const start = now();
             onStart({ key, length: data.length });
-            const scoreArray = data.slice(0, maxSize).map(d => d.score);
-            await dataStore.storeQueue({ name: key, data: scoreArray, timestamp: Date.now() });
+            const dataArray = data.slice(0, maxSize).map(d => ({ score: d.score, maxExceeded: d.maxExceeded }));
+            await dataStore.storeQueue({ name: key, data: dataArray.score, maxExceeded: dataArray.maxExceeded, timestamp: Date.now() });
             const end = now();
             const timeTook = (end - start).toFixed(3);
             onEnd({ key, length: data.length, timeTook });
