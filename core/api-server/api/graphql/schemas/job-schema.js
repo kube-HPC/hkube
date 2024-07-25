406d781cad8071c387300059c30da95262e51d03
Adir111 <adir987321@gmail.com>
2024-07-25 13:06:31 +0300

    Updated States, and reorganized file for better readability

--- core/api-server/api/graphql/schemas/job-schema.js
+++ core/api-server/api/graphql/schemas/job-schema.js
@@ -2,53 +2,60 @@ const { gql } = require('apollo-server');
 
 const jobTypeDefs = gql`
 
-scalar Object
+  scalar Object
 
+  input Range {
+    from: String
+    to: String
+  }
 
-input Range {
-  from: String,
-  to:String
-
-}
-
-
-type Results { 
+  type Results {
     startTime: Float
     pipeline: String
     status: String
     timestamp: Float
     timeTook: Float
-    data: DataWide 
-}
+    data: DataWide
+  }
 
-  type Discovery { host: String port: String }
+  type Discovery {
+    host: String
+    port: String
+  }
 
+  type Metadata {
+    values: Object
+  }
 
+  type StorageInfo {
+    path: String
+    size: Int
+  }
 
-  type Metadata { values: Object }
-
-  type StorageInfo { path: String size: Int }
-
-  type Output { 
-      taskId: String
+  type Output {
+    taskId: String
     discovery: Discovery
     metadata: Metadata
     storageInfo: StorageInfo
- }
-type  Batch {
-  taskId: String
-  podName: String
-  status: String
-  batchIndex: Int
-  startTime: String
-  endTime: String
-  output: Output
-  input: [NodeInput]
-}
-  type NodeInput { path: String ,value : Object}
+  }
 
+  type Batch {
+    taskId: String
+    podName: String
+    status: String
+    batchIndex: Int
+    startTime: String
+    endTime: String
+    output: Output
+    input: [NodeInput]
+  }
 
-  type JobNodes { 
+  type NodeInput {
+    path: String
+    value: Object
+  }
+
+  type JobNodes {
     nodeName: String
     algorithmName: String
     algorithmVersion: String
@@ -56,32 +63,31 @@ type  Batch {
     podName: String
     status: String
     startTime: Float
-    stateType:String
+    stateType: String
     endTime: Float
-    batchOperation:String
-    ttl:Int
-    kind:String
+    batchOperation: String
+    ttl: Int
+    kind: String
     level: Int
     batch: [Batch]
     batchInfo: BatchInfo
-    boards: [String ]
+    boards: [String]
     output: Output
-    input: [NodeInput ] 
+    input: [NodeInput]
     minStatelessCount: Int
     maxStatelessCount: Int
-    error:String
-    warnings:[String]
-    retries:Int
-    downloadFileExt:String
-    
+    error: String
+    warnings: [String]
+    retries: Int
+    downloadFileExt: String
   }
 
-  type PipelineForJobNodes { 
-      nodeName: String
-      algorithmName: String
-      input:[Object]
-      kind: String
-      stateType: String
+  type PipelineForJobNodes {
+    nodeName: String
+    algorithmName: String
+    input: [Object]
+    kind: String
+    stateType: String
   }
 
   type BatchInfo {
@@ -92,59 +98,110 @@ type  Batch {
     total: Int
   }
 
-  type Value { types: [String ] metrics: Object}
+  type Value {
+    types: [String]
+    metrics: Object
+  }
 
-  type Edges { from: String to: String value: Value }
+  type Edges {
+    from: String
+    to: String
+    value: Value
+  }
 
-  type Graph { jobId: String
+  type Graph {
+    jobId: String
     timestamp: Float
-    nodes: [JobNodes ]
-    edges: [Edges ] }
+    nodes: [JobNodes]
+    edges: [Edges]
+  }
 
-  type States { succeed: Int,failed:Int,stopped:Int,active:Int,creating:Int,preschedule:Int,pending:Int,skipped:Int,stalled:Int,warning:Int }
+  type States {
+    succeed: Int
+    failed: Int
+    stopped: Int
+    active: Int
+    creating: Int
+    preschedule: Int
+    pending: Int
+    skipped: Int
+    stalled: Int
+    warning: Int
+    FailedScheduling: Int
+  }
 
-  type Data { progress: Float details: String states: States  }
-  type DataWide { progress: Float details: String states: States storageInfo: StorageInfo }
+  type Data {
+    progress: Float
+    details: String
+    states: States
+  }
 
-  type Status { timestamp: Float
+  type DataWide {
+    progress: Float
+    details: String
+    states: States
+    storageInfo: StorageInfo
+  }
+
+  type Status {
+    timestamp: Float
     status: String
     level: String
     pipeline: String
-    data: Data }
+    data: Data
+  }
 
-  type LastRunResult { timestamp: String status: String timeTook: Float }
+  type LastRunResult {
+    timestamp: String
+    status: String
+    timeTook: Float
+  }
 
+  type FlowInputMetadata {
+    storageInfo: StorageInfo
+    metadata: Metadata
+  }
 
+  type Cron {
+    enabled: Boolean
+    pattern: String
+  }
 
-  type FlowInputMetadata { storageInfo: StorageInfo metadata: Metadata }
+  type Triggers {
+    cron: Cron
+  }
 
-  type Cron { enabled: Boolean pattern: String }
-
-  type Triggers { cron: Cron }
-
-  type Options { batchTolerance: Int
+  type Options {
+    batchTolerance: Int
     progressVerbosityLevel: String
-    ttl: Int }
+    ttl: Int
+  }
 
-  type Files { link: String }
+  type Files {
+    link: String
+  }
 
-  type FlowInput { files: Files }
+  type FlowInput {
+    files: Files
+  }
 
-  type PipelineForJob { name: String
+  type PipelineForJob {
+    name: String
     experimentName: String
     kind: String
     priority: Int
     startTime: Float
-    types: [String ]
+    types: [String]
     lastRunResult: LastRunResult
     flowInputMetadata: FlowInputMetadata
     triggers: Triggers
     options: Options
     flowInput: Object
-    nodes: [PipelineForJobNodes ] 
-}
+    nodes: [PipelineForJobNodes]
+  }
 
-  type UserPipeline { name: String
+  type UserPipeline {
+    name: String
     experimentName: String
     kind: String
     priority: Int
@@ -152,35 +209,42 @@ type  Batch {
     triggers: Triggers
     options: Options
     flowInput: Object
-    nodes: [JobNodes ] 
-    }
+    nodes: [JobNodes]
+  }
 
-  type Job { key: String
-    externalId:String
+  type Job {
+    key: String
+    externalId: String
     results: Results
     graph: Graph
     status: Status
     pipeline: PipelineForJob
-    userPipeline: UserPipeline 
-    cursor:String
-    timeTook:String
-    }
+    userPipeline: UserPipeline
+    cursor: String
+    timeTook: String
+  }
 
-  type AggregatedJobs { 
-    jobs:[Job]
-    cursor:String
-    jobsCount:Int 
-    }
-
+  type AggregatedJobs {
+    jobs: [Job]
+    cursor: String
+    jobsCount: Int
+  }
 
   extend type Query {
-    jobsAggregated(experimentName:String, pipelineName:String, pipelineType:String, algorithmName:String, pipelineStatus:String,datesRange:Range,cursor:String,limit:Int): AggregatedJobs
+    jobsAggregated(
+      experimentName: String
+      pipelineName: String
+      pipelineType: String
+      algorithmName: String
+      pipelineStatus: String
+      datesRange: Range
+      cursor: String
+      limit: Int
+    ): AggregatedJobs
+
     job(id: String!): Job
     jobsByExperimentName(experimentName: String!): [Job]
   }
-
-  # Types with identical fields:
-  # Green FlowInputfileslink
 
 `;
 
