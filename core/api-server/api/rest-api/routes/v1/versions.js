896e37221be566479ab88de2809f92f7a48fb31c
Adir111 <adir987321@gmail.com>
2024-09-26 08:24:54 +0300

    changed parameter name

--- core/api-server/api/rest-api/routes/v1/versions.js
+++ core/api-server/api/rest-api/routes/v1/versions.js
@@ -1,6 +1,6 @@
 const RestServer = require('@hkube/rest-server');
 const HttpStatus = require('http-status-codes');
-const versionsService = require('../../../../lib/service/algorithm-versions');
+const algoVersionsService = require('../../../../lib/service/algorithm-versions');
 
 const routes = (options) => {
     const router = RestServer.router();
@@ -10,23 +10,23 @@ const routes = (options) => {
     router.get('/algorithms/:name', async (req, res) => {
         const { name } = req.params;
         const { sort, order, limit } = req.query;
-        const response = await versionsService.getVersions({ name, sort, order, limit });
+        const response = await algoVersionsService.getVersions({ name, sort, order, limit });
         res.json(response);
     });
     router.get('/algorithms/:name/:version', async (req, res) => {
-        const response = await versionsService.getVersion(req.params);
+        const response = await algoVersionsService.getVersion(req.params);
         res.json(response);
     });
     router.post('/algorithms/apply', async (req, res) => {
-        const response = await versionsService.applyVersion(req.body);
+        const response = await algoVersionsService.applyVersion(req.body);
         res.status(HttpStatus.CREATED).json(response);
     });
     router.post('/algorithms/tag', async (req, res) => {
-        const response = await versionsService.tagVersion(req.body);
+        const response = await algoVersionsService.tagVersion(req.body);
         res.status(HttpStatus.CREATED).json(response);
     });
     router.delete('/algorithms/:name/:version', async (req, res) => {
-        const response = await versionsService.deleteVersion(req.params);
+        const response = await algoVersionsService.deleteVersion(req.params);
         res.json(response);
     });
     return router;
