d08198b3e4a216096f9239dca8eb6e7bee3b9e64
Adir111 <adir987321@gmail.com>
2024-09-26 08:26:58 +0300

    added pipeline versioning support

--- core/api-server/api/rest-api/routes/v1/versions.js
+++ core/api-server/api/rest-api/routes/v1/versions.js
@@ -1,12 +1,14 @@
 const RestServer = require('@hkube/rest-server');
 const HttpStatus = require('http-status-codes');
 const algoVersionsService = require('../../../../lib/service/algorithm-versions');
+const pipelineVersionsService = require('../../../../lib/service/algorithm-versions');
 
 const routes = (options) => {
     const router = RestServer.router();
     router.get('/', (req, res) => {
         res.json({ message: `${options.version} ${options.file} api` });
     });
+    // Algorithms
     router.get('/algorithms/:name', async (req, res) => {
         const { name } = req.params;
         const { sort, order, limit } = req.query;
@@ -29,6 +31,31 @@ const routes = (options) => {
         const response = await algoVersionsService.deleteVersion(req.params);
         res.json(response);
     });
+
+    // Pipelines
+    router.get('/pipelines/:name', async (req, res) => {
+        const { name } = req.params;
+        const { sort, order, limit } = req.query;
+        const response = await pipelineVersionsService.getVersions({ name, sort, order, limit });
+        res.json(response);
+    });
+    router.get('/pipelines/:name/:version', async (req, res) => {
+        const response = await pipelineVersionsService.getVersion(req.params);
+        res.json(response);
+    });
+    router.post('/pipelines/apply', async (req, res) => {
+        const response = await pipelineVersionsService.applyVersion(req.body);
+        res.status(HttpStatus.StatusCodes.CREATED).json(response);
+    });
+    router.post('/pipelines/tag', async (req, res) => {
+        const response = await pipelineVersionsService.tagVersion(req.body);
+        res.status(HttpStatus.StatusCodesCREATED).json(response);
+    });
+    router.delete('/pipelines/:name/:version', async (req, res) => {
+        const response = await pipelineVersionsService.deleteVersion(req.params);
+        res.json(response);
+    });
+
     return router;
 };
 
