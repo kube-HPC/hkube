f1bfb3758595673255b52b49515220a48f23a40f
Adir111 <adir987321@gmail.com>
2024-10-01 09:20:56 +0300

    added pipeline versioning

---
+++ core/api-server/lib/service/pipeline-versions.js
@@ -0,0 +1,169 @@
+const semverLib = require('semver');
+const asyncQueue = require('async.queue');
+const { uid } = require('@hkube/uid');
+const validator = require('../validation/api-validator');
+const stateManager = require('../state/state-manager');
+const { ResourceNotFoundError, ActionNotAllowed } = require('../errors');
+
+const SETTINGS = {
+    SEMVER: {
+        FIRST: '1.0.0',
+        MAX_PATCH: 500,
+        MAX_MINOR: 500,
+        MAX_MAJOR: 500
+    },
+    VERSION_LENGTH: 10
+};
+
+class PipelineVersions {
+    constructor() {
+        this._versionsQueue = asyncQueue((task, callback) => {
+            this._createVersion(task)
+                .then(r => callback(null, r))
+                .catch(e => callback(e));
+        }, 1);
+    }
+
+    async getVersions(options) {
+        validator.pipelines.validatePipelineName(options);
+        const { name } = options;
+        const versions = await stateManager.getVersions({ name }, true);
+        return versions;
+    }
+
+    async getVersion({ name, version }) {
+        const pipeline = await stateManager.getPipeline({ name });
+        if (!pipeline) {
+            throw new ResourceNotFoundError('pipeline', name);
+        }
+        const pipelineVersion = await this._getVersion({ version });
+        if (!pipelineVersion) {
+            throw new ResourceNotFoundError('version', version);
+        }
+        return pipelineVersion;
+    }
+
+    async tagVersion(options) {
+        const { name, version, pinned, tags } = options;
+        validator.pipelines.validatePipelineTag(options);
+        const ver = await this.getVersion({ name, version });
+        await stateManager.updateVersion({ version, pinned, tags }, true);
+        return ver;
+    }
+
+    async applyVersion(options) {
+        const { name, version, force } = options;
+        validator.pipelines.validatePipelineVersion(options);
+        const pipelineVersion = await this.getVersion({ name, version });
+
+        // check if running pipelines
+        if (!force) {
+            const runningPipelines = await stateManager.searchJobs({ pipelineName: name, hasResult: false, fields: { jobId: true } });
+            if (runningPipelines.length > 0) {
+                throw new ActionNotAllowed(`The pipeline "${name}" is currently running`, runningPipelines.map(p => p.jobId));
+            }
+        }
+
+        await stateManager.updatePipeline(pipelineVersion.pipeline);
+        return pipelineVersion;
+    }
+
+    async deleteVersion(options) {
+        const { version, name } = options;
+        validator.pipelines.validatePipelineVersion({ name, version });
+        const pipeline = await stateManager.getPipeline({ name });
+        if (!pipeline) {
+            throw new ResourceNotFoundError('pipeline', name);
+        }
+        if (pipeline.version === version) {
+            throw new ActionNotAllowed('unable to remove used version');
+        }
+        const pipelineVersion = await this._getVersion({ version });
+        if (!pipelineVersion) {
+            throw new ResourceNotFoundError('version', version);
+        }
+        const res = await stateManager.deleteVersion({ name, version }, true);
+        const deleted = parseInt(res.deleted, 10);
+        return { deleted };
+    }
+
+    /**
+     * This method creates new pipeline version.
+     * Version is created for any change in pipeline.
+     * The version has a two important properties that automatically generated.
+     *  1. version: <string> (10 length uid).
+     *  2. semver:  <string> (major, minor, patch).
+     * The algorithm to create version is:
+     * 1) generate uid.
+     * 2) get the latest semver from versions list.
+     * 3) increment the semver.
+     * 4) try to acquire lock, so no other user will get the same semver at the same exact time.
+     * 5) if lock was unsuccessful, try to increment the semver again.
+     * 6) create the version.
+     */
+    async createVersion(pipeline) {
+        return new Promise((resolve, reject) => {
+            this._versionsQueue.push({ pipeline }, (err, res) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(res);
+            });
+        });
+    }
+
+    async _createVersion({ pipeline }) {
+        const { name } = pipeline;
+        const version = uid({ length: SETTINGS.VERSION_LENGTH });
+        const latestSemver = await this._getLatestSemver({ name });
+        const semver = this._incSemver(latestSemver);
+        const newVersion = {
+            version,
+            semver,
+            created: Date.now(),
+            name,
+            pipeline: { ...pipeline, version }
+        };
+        await stateManager.createVersion(newVersion, true);
+        return version;
+    }
+
+    async _getLatestSemver({ name }) {
+        const versions = await stateManager.getVersions({ name, limit: 1 }, true);
+        return versions?.[0]?.semver;
+    }
+
+    async _getVersion({ version }) {
+        const algorithmVersion = await stateManager.getVersion({ version }, true);
+        return algorithmVersion;
+    }
+
+    _incSemver(oldVersion) {
+        let version;
+
+        if (!oldVersion) {
+            version = SETTINGS.SEMVER.FIRST;
+        }
+        else {
+            const ver = semverLib.valid(oldVersion);
+            if (!ver) {
+                version = oldVersion;
+            }
+            else {
+                const { patch, minor, major } = semverLib.parse(oldVersion);
+                if (patch < SETTINGS.SEMVER.MAX_PATCH) {
+                    version = semverLib.inc(oldVersion, 'patch');
+                }
+                else if (minor < SETTINGS.SEMVER.MAX_MINOR) {
+                    version = semverLib.inc(oldVersion, 'minor');
+                }
+                else if (major < SETTINGS.SEMVER.MAX_MAJOR) {
+                    version = semverLib.inc(oldVersion, 'major');
+                }
+            }
+        }
+        return version;
+    }
+}
+
+module.exports = new PipelineVersions();
