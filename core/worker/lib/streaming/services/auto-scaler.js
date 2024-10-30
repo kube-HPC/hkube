4c3d230f3f2d56ee1b1f647efd8811e8a508d5c2
Adir111 <adir987321@gmail.com>
2024-10-30 16:38:03 +0200

    removed for checking

--- core/worker/lib/streaming/services/auto-scaler.js
+++ core/worker/lib/streaming/services/auto-scaler.js
@@ -129,14 +129,15 @@ class AutoScaler {
             });
             stats[source].windowSize.push(windowSize);
             stats[source].rates.push(rates);
-
-            const newMaxWindowSize = Math.ceil(rates.resRate * 2);
-            this._statistics.updateMaxWindowSize({
-                source: stat.source,
-                durations: newMaxWindowSize,
-                grossDurations: newMaxWindowSize,
-                queueDurations: newMaxWindowSize
-            });
+            if (stats === 'adir') {
+                const newMaxWindowSize = Math.ceil(rates.resRate * 2);
+                this._statistics.updateMaxWindowSize({
+                    source: stat.source,
+                    durations: newMaxWindowSize,
+                    grossDurations: newMaxWindowSize,
+                    queueDurations: newMaxWindowSize
+                });
+            }
         });
 
         const totals = {
