2b1f23b70c33cee860162872d2018c33884a6f3b
Adir111 <adir987321@gmail.com>
2024-12-19 14:54:58 +0200

    added tests for algorithm spec with pvc/configmap/secret exist/no exist, and test for emptyDir

--- core/task-executor/tests/mocks/kubernetes.mock.js
+++ core/task-executor/tests/mocks/kubernetes.mock.js
@@ -18,9 +18,9 @@ module.exports = {
                 getVersionsConfigMap: async () => { return {}; },
                 getResourcesPerNode: async () => { return {}; },
                 getSidecarConfigs: async () => { return {}; },
-                getAllPVCNames: async () => { return []; },
-                getAllSecretNames: async () => { return []; },
-                getAllConfigMapNames: async () => { return []; }
+                getAllPVCNames: async () => { return ['pvc-1', 'pvc-2']; },
+                getAllSecretNames: async () => { return ['secret-1', 'secret-2']; },
+                getAllConfigMapNames: async () => { return ['config-map-1', 'config-map-2']; }
             },
             callCount: (name) => {
                 return callCount[name];
