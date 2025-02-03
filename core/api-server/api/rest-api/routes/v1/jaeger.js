ca5d8ccb3bc10de423b27e1c345bfb39a25757ef
ronshv <lepilmen@gmail.com>
2025-02-03 15:18:00 +0200

    jaeger using keycloak protect

--- core/api-server/api/rest-api/routes/v1/jaeger.js
+++ core/api-server/api/rest-api/routes/v1/jaeger.js
@@ -1,9 +1,11 @@
 const RestServer = require('@hkube/rest-server');
+const { keycloakRoles } = require('@hkube/consts');
 const { pipeTrace } = require('../../../../lib/service/jaeger-api');
+const keycloak = require('../../../../lib/service/keycloak');
 
 const routes = () => {
     const router = RestServer.router();
-    router.get('/', async (req, res) => {
+    router.get('/', keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         await pipeTrace(req.query.jobId, res);
     });
 
