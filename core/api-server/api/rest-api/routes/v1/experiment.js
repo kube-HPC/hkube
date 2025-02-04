ee3cf0651e2d0d0d8d92b46fdbb1a5c90d3d24b1
ronshv <lepilmen@gmail.com>
2025-02-04 09:12:18 +0200

    experiment using keycloak protect

--- core/api-server/api/rest-api/routes/v1/experiment.js
+++ core/api-server/api/rest-api/routes/v1/experiment.js
@@ -1,24 +1,26 @@
 const RestServer = require('@hkube/rest-server');
+const { keycloakRoles } = require('@hkube/consts');
 const Experiment = require('../../../../lib/service/experiment');
+const keycloak = require('../../../../lib/service/keycloak');
 
 const routes = () => {
     const router = RestServer.router();
-    router.get('/', async (req, res) => {
+    router.get('/', keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         const { sort, order, limit } = req.query;
         const response = await Experiment.experimentsList({ sort, order, limit });
         res.json(response);
     });
-    router.get('/:name?', async (req, res) => {
+    router.get('/:name?', keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         const { name } = req.params;
         const response = await Experiment.getExperiment({ name });
         res.json(response);
     });
-    router.post('/', async (req, res) => {
+    router.post('/', keycloak.getProtect(keycloakRoles.API_EDIT), async (req, res) => {
         const { name, description } = req.body;
         await Experiment.insertExperiment({ name, description });
         res.json({ message: 'OK', name });
     });
-    router.delete('/:name?', async (req, res) => {
+    router.delete('/:name?', keycloak.getProtect(keycloakRoles.API_DELETE), async (req, res) => {
         const { name } = req.params;
         const response = await Experiment.deleteExperiment({ name });
         res.json(response);
