03c7af5dfba0b06aa10abbd75de54525d0b33789
ronshv <lepilmen@gmail.com>
2025-02-04 09:29:47 +0200

    cron using keycloak protect

--- core/api-server/api/rest-api/routes/v1/cron.js
+++ core/api-server/api/rest-api/routes/v1/cron.js
@@ -1,33 +1,35 @@
 const RestServer = require('@hkube/rest-server');
+const { keycloakRoles } = require('@hkube/consts');
 const Cron = require('../../../../lib/service/cron');
 const methods = require('../../middlewares/methods');
+const keycloak = require('../../../../lib/service/keycloak');
 
 const routes = (options) => {
     const router = RestServer.router();
-    router.get('/', (req, res) => {
+    router.get('/', keycloak.getProtect(keycloakRoles.API_VIEW), (req, res) => {
         res.json({ message: `${options.version} ${options.file} api` });
     });
-    router.all('/results', methods(['GET']), async (req, res) => {
+    router.all('/results', methods(['GET']), keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         const { experimentName, name, sort, order, limit } = req.query;
         const response = await Cron.getCronResult({ experimentName, name, sort, order, limit });
         res.json(response);
     });
-    router.all('/status', methods(['GET']), async (req, res) => {
+    router.all('/status', methods(['GET']), keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         const { experimentName, name, sort, order, limit } = req.query;
         const response = await Cron.getCronStatus({ experimentName, name, sort, order, limit });
         res.json(response);
     });
-    router.all('/list/:name?', methods(['GET']), async (req, res) => {
+    router.all('/list/:name?', methods(['GET']), keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         const { sort, order, limit } = req.query;
         const response = await Cron.getCronList({ sort, order, limit });
         res.json(response);
     });
-    router.post('/start', async (req, res) => {
+    router.post('/start', keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         const { name, pattern } = req.body;
         await Cron.startCronJob({ name, pattern });
         res.json({ message: 'OK' });
     });
-    router.post('/stop', async (req, res) => {
+    router.post('/stop', keycloak.getProtect(keycloakRoles.API_VIEW), async (req, res) => {
         const { name, pattern } = req.body;
         await Cron.stopCronJob({ name, pattern });
         res.json({ message: 'OK' });
