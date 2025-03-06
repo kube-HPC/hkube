68126b205f57b9b8f6856e6716d389b77faef947
Adir111 <adir987321@gmail.com>
2025-03-06 16:22:01 +0200

    updated mock to support cases of algorithm failures in createJob

--- core/task-executor/tests/mocks/kubernetes.mock.js
+++ core/task-executor/tests/mocks/kubernetes.mock.js
@@ -13,6 +13,24 @@ module.exports = {
                 init: async () => { },
                 createJob: async (...theArgs) => {
                     registerCount('createJob', theArgs);
+                    const supposeToFail = [
+                        { algorithmName: 'algo-car-lim-lower-req', error: 'Job is invalid: mycar.resources.requests: Invalid value: 2: must be less than or equal to cpu limit', statusCode: 422 }
+                    ];
+                    const jobDetails = theArgs[0]?.jobDetails;
+                    const failureEntry = supposeToFail.find(entry => entry.algorithmName === jobDetails?.algorithmName);
+
+                    if (failureEntry) {
+                        return {
+                            job: jobDetails,
+                            statusCode: failureEntry.statusCode,
+                            error: failureEntry.error
+                        };
+                    }
+                
+                    return {
+                        statusCode: 200,
+                        job: jobDetails
+                    };
                 },
                 getWorkerJobs: async () => { },
                 getVersionsConfigMap: async () => { return {}; },
