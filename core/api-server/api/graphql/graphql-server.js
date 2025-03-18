2fc989bb0a798bcea329cc6b6d10f462bec9b3bd
Adir111 <adir987321@gmail.com>
2025-03-18 10:18:44 +0200

    error formatting

--- core/api-server/api/graphql/graphql-server.js
+++ core/api-server/api/graphql/graphql-server.js
@@ -47,6 +47,25 @@ async function startApolloServer(typeDefs, resolvers, app, httpServer, port, con
                 }
                 return context;
             },
+            formatError: (err) => {
+                if (err.originalError instanceof AuthenticationError) {
+                    const code = (() => {
+                        switch (err.originalError.status) {
+                        case HttpStatus.StatusCodes.FORBIDDEN: return 'FORBIDDEN';
+                        case HttpStatus.StatusCodes.UNAUTHORIZED: return 'UNAUTHORIZED';
+                        default: return 'INTERNAL_SERVER_ERROR';
+                        }
+                    })();
+                    return {
+                        message: err.message,
+                        extensions: {
+                            code,
+                            status: err.originalError.status
+                        }
+                    };
+                }
+                return err;
+            },
             plugins: [{
                 async serverWillStart() {
                     return {
