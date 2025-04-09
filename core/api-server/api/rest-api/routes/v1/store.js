171394738689a1f92e18203c002187e07ddd96a8
Adir David <118291963+Adir111@users.noreply.github.com>
2025-04-09 16:01:53 +0300

    Post put old algorithm store support (#2126)
    * added a support for old ways
    
    * added old way support
    
    * supports old ways
    
    * moved test
    
    * added both old way and new way tests
    
    * removed .only
    
    * disable keycloak for testing
    
    * returning only algortihm like in old way, or error if there is any

--- core/api-server/api/rest-api/routes/v1/store.js
+++ core/api-server/api/rest-api/routes/v1/store.js
@@ -98,22 +98,47 @@ const routes = (option) => {
     router.post('/algorithms', keycloak.getProtect(keycloakRoles.API_EDIT), upload.single('file'), async (req, res) => {
         const { file, body } = req;
         try {
-            if (Array.isArray(body.payload)) {
-                const returnAlgoList = await Promise.all(
-                    body.payload.map(async (algorithmData) => {
-                        const { payload, options } = await _processPayLoadAndOptions(algorithmData, body.options);
-                        options.failOnError = false;
-                        const response = await algorithmStore.insertAlgorithm({ payload, options });
-                        return response;
-                    })
-                );
-                res.status(HttpStatus.StatusCodes.CREATED).json(returnAlgoList);
+            if (body.payload !== undefined) { // New way
+                if (Array.isArray(body.payload)) {
+                    const returnAlgoList = await Promise.all(
+                        body.payload.map(async (algorithmData) => {
+                            const { payload, options } = await _processPayLoadAndOptions(algorithmData, body.options);
+                            options.failOnError = false;
+                            const response = await algorithmStore.insertAlgorithm({ payload, options });
+                            return response;
+                        })
+                    );
+                    res.status(HttpStatus.StatusCodes.CREATED).json(returnAlgoList);
+                }
+                else {
+                    // If req.body.payload is not an array, process it as a single algorithm
+                    const { payload, options } = await _processPayLoadAndOptions(body.payload, body.options);
+                    const response = await algorithmStore.insertAlgorithm({ payload, options, file });
+                    res.status(HttpStatus.StatusCodes.CREATED).json(response);
+                }
             }
-            else {
-                // If req.body.payload is not an array, process it as a single algorithm
-                const { payload, options } = await _processPayLoadAndOptions(body.payload, body.options);
-                const response = await algorithmStore.insertAlgorithm({ payload, options, file });
-                res.status(HttpStatus.StatusCodes.CREATED).json(response);
+            else { // Old way
+                const allowOverwrite = req.query.overwrite;
+                if (Array.isArray(req.body)) {
+                    const returnAlgoList = await Promise.all(
+                        req.body.map(async (algorithmData) => {
+                            const payload = algorithmData;
+                            const options = { failOnError: false, allowOverwrite };
+                            const response = await algorithmStore.insertAlgorithm({ payload, options });
+                            const { algorithm } = response;
+                            return algorithm || response;
+                        })
+                    );
+                    res.status(HttpStatus.StatusCodes.CREATED).json(returnAlgoList);
+                }
+                else {
+                    // If req.body is not an array, process it as a single algorithm
+                    const payload = req.body;
+                    const options = { failOnError: true, allowOverwrite };
+                    const response = await algorithmStore.insertAlgorithm({ payload, options });
+                    const { algorithm } = response;
+                    res.status(HttpStatus.StatusCodes.CREATED).json(algorithm || response);
+                }
             }
         }
         finally {
@@ -125,9 +150,19 @@ const routes = (option) => {
     router.put('/algorithms', keycloak.getProtect(keycloakRoles.API_EDIT), upload.single('file'), async (req, res) => {
         const { file, body } = req;
         try {
-            const { payload, options } = await _processPayLoadAndOptions(body.payload, body.options);
-            const response = await algorithmStore.updateAlgorithm({ payload, options, file });
-            res.json(response);
+            if (body.payload !== undefined) { // New way
+                const { payload, options } = await _processPayLoadAndOptions(body.payload, body.options);
+                const response = await algorithmStore.updateAlgorithm({ payload, options, file });
+                res.json(response);
+            }
+            else { // Old way
+                const forceUpdate = req?.query?.forceStopAndApplyVersion === 'true';
+                const payload = req.body;
+                const options = { forceUpdate };
+                const response = await algorithmStore.updateAlgorithm({ payload, options });
+                const { algorithm } = response;
+                res.json(algorithm || response);
+            }
         }
         finally {
             if (file?.path) {
