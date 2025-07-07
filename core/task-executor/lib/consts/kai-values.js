ee1faecf03fb252cc7e57426fb8d51d8211ebe27
Adir David <adir987321@gmail.com>
2025-07-07 14:52:26 +0300

    added new consts for kai scheduler

---
+++ core/task-executor/lib/consts/kai-values.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ANNOTATIONS: {
+        FRACTION: 'gpu-fraction',
+        MEMORY: 'gpu-memory'
+    },
+    LABELS: {
+        QUEUE: 'runai/queue'
+    },
+    SCHEDULER_NAME: 'kai-scheduler'
+};
