c98f350e8147c741544060c43c63eeff839a3165
Adir David <adir987321@gmail.com>
2025-07-08 14:51:41 +0300

    added values for k8s for kai

--- core/task-executor/lib/consts/kai-values.js
+++ core/task-executor/lib/consts/kai-values.js
@@ -6,5 +6,13 @@ module.exports = {
     LABELS: {
         QUEUE: 'runai/queue'
     },
-    SCHEDULER_NAME: 'kai-scheduler'
+    SCHEDULER_NAME: 'kai-scheduler',
+    KUBERNETES: {
+        CRD_GROUP: 'apiextensions.k8s.io',
+        CRD_VERSION: 'v1',
+        CRD_RESOURCE: 'customresourcedefinitions',
+        QUEUES_CRD_NAME: 'queues.scheduling.run.ai',
+        QUEUES_API_GROUP: 'scheduling.run.ai',
+        RESOURCE: 'queues'
+    }
 };
