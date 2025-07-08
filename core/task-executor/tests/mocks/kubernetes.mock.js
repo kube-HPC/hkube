c534c30e22a1ed76345b80badab810b6fe8ecbde
Adir David <adir987321@gmail.com>
2025-07-08 14:56:36 +0300

    added mock for queues

--- core/task-executor/tests/mocks/kubernetes.mock.js
+++ core/task-executor/tests/mocks/kubernetes.mock.js
@@ -39,7 +39,8 @@ module.exports = {
                 getSidecarConfigs: async () => { return {}; },
                 getAllPVCNames: async () => { return ['pvc-1', 'pvc-2']; },
                 getAllSecretNames: async () => { return ['secret-1', 'secret-2']; },
-                getAllConfigMapNames: async () => { return ['config-map-1', 'config-map-2']; }
+                getAllConfigMapNames: async () => { return ['config-map-1', 'config-map-2']; },
+                getAllQueueNames: async () => { return ['test', 'default']; },
             },
             callCount: (name) => {
                 return callCount[name];
