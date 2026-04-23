ebfe5d30c0cb9ea80a8d77191bddd4b558b4f165
Itamar <itsh13@gmail.com>
2026-04-23 14:54:12 +0300

    Add jobId label to pipeline net and gross time metrics and remove commented-out code

--- core/pipeline-driver/lib/metrics/pipeline-metrics.js
+++ core/pipeline-driver/lib/metrics/pipeline-metrics.js
@@ -68,27 +68,17 @@ class PipelineMetrics {
             description: 'Required Pods',
             labels: ['pipelineName', 'jobId', 'source', 'target']
         });
-        // metrics.addGaugeMeasure({
-        //     name: metricsNames.pipeline_net_time_took,
-        //     description: 'Pipeline net time took in seconds',
-        //     labels: ['pipeline_name', 'status']
-        // });
-        // metrics.addGaugeMeasure({
-        //     name: metricsNames.pipeline_gross_time_took,
-        //     description: 'Pipeline gross time took in seconds',
-        //     labels: ['pipeline_name', 'status']
-        // });
         metrics.addTimeMeasure({
             name: metricsNames.pipeline_net_time_took,
             description: 'Pipeline net time took in seconds',
-            labels: ['pipeline_name', 'status'],
+            labels: ['pipeline_name', 'status', 'jobId'],
             buckets: utils.arithmatcSequence(30, 0, 2)
                 .concat(utils.geometricSequence(10, 56, 2, 1).slice(2)).map(i => i * 1000)
         });
         metrics.addTimeMeasure({
             name: metricsNames.pipeline_gross_time_took,
             description: 'Pipeline gross time took in seconds',
-            labels: ['pipeline_name', 'status'],
+            labels: ['pipeline_name', 'status', 'jobId'],
             buckets: utils.arithmatcSequence(30, 0, 2)
                 .concat(utils.geometricSequence(10, 56, 2, 1).slice(2)).map(i => i * 1000)
         });
@@ -162,22 +152,14 @@ class PipelineMetrics {
                 }
             });
             if (netTimeTook != null) {
-                // metrics.get(metricsNames.pipeline_net_time_took).set({
-                //     labelValues: { pipeline_name: pipeline, status },
-                //     value: netTimeTook
-                // });
                 metrics.get(metricsNames.pipeline_net_time_took).retroactive({
-                    labelValues: { pipeline_name: pipeline, status },
+                    labelValues: { pipeline_name: pipeline, status, jobId },
                     time: netTimeTook * 1000
                 });
             }
             if (grossTimeTook != null) {
-                // metrics.get(metricsNames.pipeline_gross_time_took).set({
-                //     labelValues: { pipeline_name: pipeline, status },
-                //     value: grossTimeTook
-                // });
                 metrics.get(metricsNames.pipeline_gross_time_took).retroactive({
-                    labelValues: { pipeline_name: pipeline, status },
+                    labelValues: { pipeline_name: pipeline, status, jobId },
                     time: grossTimeTook * 1000
                 });
             }
